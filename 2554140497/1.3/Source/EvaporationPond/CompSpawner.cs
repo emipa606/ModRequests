2bae3608b9dd31b3a6d8bedf7494dd561dabfd14
Mlie <mlie@litet.net>
2023-02-01 22:37:17 +0100

    Evaporation Pond

---
+++ 2554140497/1.3/Source/EvaporationPond/CompSpawner.cs
@@ -0,0 +1,195 @@
+﻿using RimWorld;
+using System.Collections.Generic;
+using Verse;
+
+namespace EvaporationPond
+{
+    public class CompSpawner : ThingComp
+    {
+        private int ticksUntilSpawn;
+
+        public CompProperties_Spawner PropsSpawner => (CompProperties_Spawner)props;
+
+        private bool PowerOn => parent.GetComp<CompPowerTrader>()?.PowerOn ?? false;
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            if (!respawningAfterLoad)
+            {
+                ResetCountdown();
+            }
+        }
+
+        public override void CompTick()
+        {
+            TickInterval(1);
+        }
+
+        public override void CompTickRare()
+        {
+            TickInterval(250);
+        }
+
+        private void TickInterval(int interval)
+        {
+            if (!parent.Spawned)
+            {
+                return;
+            }
+            CompCanBeDormant comp = parent.GetComp<CompCanBeDormant>();
+            if (comp != null)
+            {
+                if (!comp.Awake)
+                {
+                    return;
+                }
+            }
+            else if (parent.Position.Fogged(parent.Map))
+            {
+                return;
+            }
+            if (!PropsSpawner.requiresPower || PowerOn)
+            {
+                ticksUntilSpawn -= interval;
+                CheckShouldSpawn();
+            }
+        }
+
+        private void CheckShouldSpawn()
+        {
+            if (ticksUntilSpawn <= 0)
+            {
+                ResetCountdown();
+                TryDoSpawn();
+            }
+        }
+
+        public bool TryDoSpawn()
+        {
+            if (!parent.Spawned)
+            {
+                return false;
+            }
+            if (PropsSpawner.spawnMaxAdjacent >= 0)
+            {
+                int num = 0;
+                for (int i = 0; i < 9; i++)
+                {
+                    IntVec3 c = parent.Position + GenAdj.AdjacentCellsAndInside[i];
+                    if (!c.InBounds(parent.Map))
+                    {
+                        continue;
+                    }
+                    List<Thing> thingList = c.GetThingList(parent.Map);
+                    for (int j = 0; j < thingList.Count; j++)
+                    {
+                        if (thingList[j].def == PropsSpawner.thingToSpawn)
+                        {
+                            num += thingList[j].stackCount;
+                            if (num >= PropsSpawner.spawnMaxAdjacent)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            if (TryFindSpawnCell(parent, PropsSpawner.thingToSpawn, PropsSpawner.spawnCount, out var result))
+            {
+                Thing thing = ThingMaker.MakeThing(PropsSpawner.thingToSpawn);
+                thing.stackCount = PropsSpawner.spawnCount;
+                if (thing == null)
+                {
+                    Log.Error("Could not spawn anything for " + parent);
+                }
+                if (PropsSpawner.inheritFaction && thing.Faction != parent.Faction)
+                {
+                    thing.SetFaction(parent.Faction);
+                }
+                GenPlace.TryPlaceThing(thing, result, parent.Map, ThingPlaceMode.Direct, out var lastResultingThing);
+                if (PropsSpawner.showMessageIfOwned && parent.Faction == Faction.OfPlayer)
+                {
+                    Messages.Message("MessageCompSpawnerSpawnedItem".Translate(PropsSpawner.thingToSpawn.LabelCap), thing, MessageTypeDefOf.PositiveEvent);
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public static bool TryFindSpawnCell(Thing parent, ThingDef thingToSpawn, int spawnCount, out IntVec3 result)
+        {
+            foreach (IntVec3 item in GenAdj.CellsAdjacent8Way(parent).InRandomOrder())
+            {
+                parent.Map.debugDrawer.FlashCell(item);
+                if (!item.Walkable(parent.Map))
+                {
+                    continue;
+                }
+                Building edifice = item.GetEdifice(parent.Map);
+                if (edifice != null && thingToSpawn.IsEdifice())
+                {
+                    continue;
+                }
+                Building_Door building_Door = edifice as Building_Door;
+                if (building_Door != null && !building_Door.FreePassage)
+                {
+                    continue;
+                }
+                bool flag = false;
+                List<Thing> thingList = item.GetThingList(parent.Map);
+                for (int i = 0; i < thingList.Count; i++)
+                {
+                    Thing thing = thingList[i];
+                    if (thing.def.category == ThingCategory.Item && (thing.def != thingToSpawn || thing.stackCount > thingToSpawn.stackLimit - spawnCount))
+                    {
+                        flag = true;
+                        break;
+                    }
+                }
+                if (!flag)
+                {
+                    result = item;
+                    return true;
+                }
+            }
+            result = IntVec3.Invalid;
+            return false;
+        }
+
+        private void ResetCountdown()
+        {
+            ticksUntilSpawn = PropsSpawner.spawnIntervalRange.RandomInRange;
+        }
+
+        public override void PostExposeData()
+        {
+            string text = (PropsSpawner.saveKeysPrefix.NullOrEmpty() ? null : (PropsSpawner.saveKeysPrefix + "_"));
+            Scribe_Values.Look(ref ticksUntilSpawn, text + "ticksUntilSpawn", 0);
+        }
+
+        public override IEnumerable<Gizmo> CompGetGizmosExtra()
+        {
+            if (Prefs.DevMode)
+            {
+                Command_Action command_Action = new Command_Action();
+                command_Action.defaultLabel = "DEBUG: Spawn " + PropsSpawner.thingToSpawn.label;
+                command_Action.icon = TexCommand.DesirePower;
+                command_Action.action = delegate
+                {
+                    ResetCountdown();
+                    TryDoSpawn();
+                };
+                yield return command_Action;
+            }
+        }
+
+        public override string CompInspectStringExtra()
+        {
+            if (PropsSpawner.writeTimeLeftToSpawn && (!PropsSpawner.requiresPower || PowerOn))
+            {
+                return "NextSpawnedItemIn".Translate(GenLabel.ThingLabel(PropsSpawner.thingToSpawn, null, PropsSpawner.spawnCount)).Resolve() + ": " + ticksUntilSpawn.ToStringTicksToPeriod().Colorize(ColoredText.DateTimeColor);
+            }
+            return null;
+        }
+    }
+}
---
+++ 2554140497/1.3/Source/EvaporationPond/PlaceWorker_EvaporationPond.cs
@@ -0,0 +1,73 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace EvaporationPond
+{
+    public class PlaceWorker_EvaporationPond : PlaceWorker
+    {
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			var groundCells = GroundCells(checkingDef, loc, out var waterCells);
+			if (waterCells is null || waterCells.Any(x => !IsShallowOceanWater(x, map)))
+			{
+				return new AcceptanceReport("EP.MustBeOnShallowOceanWater".Translate());
+			}
+			else if (groundCells is null || groundCells.Any(x => IsWater(x, Find.CurrentMap)))
+			{
+				return new AcceptanceReport("EP.MustBeOnNonWaterGround".Translate());
+			}
+			return true;
+		}
+		private bool IsWater(IntVec3 loc, Map map)
+		{
+			return map.terrainGrid.TerrainAt(loc).IsWater;
+		}
+		private bool IsShallowOceanWater(IntVec3 loc, Map map)
+		{
+			return map.terrainGrid.TerrainAt(loc) == TerrainDefOf.WaterOceanShallow;
+		}
+		public List<IntVec3> GroundCells(BuildableDef checkingDef, IntVec3 loc, out List<IntVec3> waterCells)
+		{
+			IEnumerable<Rot4> RotationsToUse()
+			{
+				yield return new Rot4(0);
+				yield return new Rot4(1);
+				yield return new Rot4(2);
+				yield return new Rot4(3);
+			}
+			var baseRect = GenAdj.OccupiedRect(loc, checkingDef.defaultPlacingRot, checkingDef.Size);
+			foreach (var rot4 in RotationsToUse())
+            {
+				bool foundWater = true;
+				waterCells = baseRect.GetEdgeCells(rot4).ToList();
+				if (waterCells.Any(x => !IsShallowOceanWater(x, Find.CurrentMap)))
+                {
+					foundWater = false;
+				}
+				if (foundWater)
+                {
+					var copy = waterCells.ListFullCopy();
+					var groundCells = baseRect.Cells.Where(x => !copy.Contains(x));
+					return groundCells.ToList();
+				}
+			}
+			waterCells = null;
+			return null;
+		}
+		public override void DrawGhost(ThingDef def, IntVec3 loc, Rot4 rot, Color ghostCol, Thing thing = null)
+		{
+			var groundCells = GroundCells(def, loc, out var waterCells);
+			if (groundCells != null)
+            {
+				GenDraw.DrawFieldEdges(groundCells.ToList(), Designator_Place.CanPlaceColor.ToOpaque());
+				GenDraw.DrawFieldEdges(waterCells, Designator_Place.CanPlaceColor.ToOpaque());
+            }
+		}
+	}
+}
