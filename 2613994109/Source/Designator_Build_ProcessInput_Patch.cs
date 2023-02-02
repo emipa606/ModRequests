3cbcd93c939cd74ff8a306168d48add08030d507
Mlie <mlie@litet.net>
2023-02-02 17:01:04 +0100

    I Have Materials

---
+++ 2613994109/Source/Designator_Build_ProcessInput_Patch.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace IHaveMaterials
+{
+    [HarmonyPatch]
+    static class Designator_Build_ProcessInput_Patch
+    {
+        private static readonly MethodInfo CheckCanInteract = AccessTools.Method(typeof(Designator_Build), "CheckCanInteract");
+        private static readonly FieldInfo stuffDef = AccessTools.Field(typeof(Designator_Build), "stuffDef");
+        private static readonly FieldInfo writeStuff = AccessTools.Field(typeof(Designator_Build), "writeStuff");
+
+        [HarmonyPatch(typeof(Designator_Build), nameof(Designator_Build.ProcessInput), new Type[] { typeof(Event) })]
+        [HarmonyPrefix]
+        static bool Prefix(Designator_Build __instance, BuildableDef ___entDef, Event ev)
+        {
+            if (!(bool)CheckCanInteract.Invoke(__instance, new object[0]))
+            {
+                return true;
+            }
+
+            var thingDef = ___entDef as ThingDef;
+            if (thingDef == null || !thingDef.MadeFromStuff)
+            {
+                return true;
+            }
+
+            var vanilla = __instance.Map.resourceCounter.AllCountedAmounts.Keys.Where(d => d.IsStuff && d.stuffProps.CanMake(thingDef) && (DebugSettings.godMode || __instance.Map.listerThings.ThingsOfDef(d).Count > 0));
+
+            // Non-vanilla stuff begins
+            var forced = IHaveMaterials.Instance.Stuff.Where(d => d.Value && d.Key.stuffProps.CanMake(thingDef)).Select(d => d.Key);
+            if (IHaveMaterials.Instance.StonyFromMap)
+            {
+                var rockWalls = Find.World.NaturalRockTypesIn(__instance.Map.Tile);
+                var rockChunks = rockWalls.Select(t => t.building.mineableThing);
+                var rockBlocks = rockChunks.SelectMany(d => d.butcherProducts.Select(p => p.thingDef));
+                forced = forced.Union(rockChunks.Concat(rockBlocks).Where(d => d.IsStuff && d.stuffProps.CanMake(thingDef)));
+            }
+
+            var merged = vanilla.Union(forced).OrderByDescending(d => d.stuffProps.commonality).ThenBy(d => d.BaseMarketValue);
+            // Non-vanilla stuff ends
+
+            var options = merged.Select(item =>
+            {
+                string label;
+
+                if (__instance.sourcePrecept == null)
+                    label = GenLabel.ThingLabel(___entDef, item);
+                else 
+                    label = "ThingMadeOfStuffLabel".Translate(item.LabelAsStuff, __instance.sourcePrecept.Label);
+
+                return new FloatMenuOption(label.CapitalizeFirst(), delegate
+                {
+                    BaseProcessInput(__instance, ev);
+                    Find.DesignatorManager.Select(__instance);
+                    stuffDef.SetValue(__instance, item);
+                    writeStuff.SetValue(__instance, true);
+                }, item)
+                {
+                    tutorTag = "SelectStuff-" + thingDef.defName + "-" + item.defName
+                };
+            }).ToList();
+
+            if (options.Count == 0)
+            {
+                return true;
+            }
+
+            FloatMenu floatMenu = new FloatMenu(options);
+            floatMenu.vanishIfMouseDistant = true;
+            floatMenu.onCloseCallback = delegate
+            {
+                writeStuff.SetValue(__instance, true);
+            };
+            Find.WindowStack.Add(floatMenu);
+            Find.DesignatorManager.Select(__instance);
+
+            return false;
+        }
+
+        [HarmonyReversePatch]
+        [HarmonyPatch(typeof(Designator), nameof(Designator.ProcessInput), new Type[] { typeof(Event) })]
+        static void BaseProcessInput(Designator instance, Event ev)
+        {
+            throw new NotImplementedException("reverse patch somehow failed");
+        }
+    }
+}
---
+++ 2613994109/Source/IHaveMaterials.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections.Generic;
+using HugsLib;
+using HugsLib.Settings;
+using HugsLib.Utils;
+using Verse;
+
+namespace IHaveMaterials
+{
+    public class IHaveMaterials : ModBase
+    {
+        public override string ModIdentifier => "me.lubar.IHaveMaterials";
+
+        internal static IHaveMaterials Instance;
+        internal static ModLogger InstanceLogger => Instance.Logger;
+
+        public SettingHandle<bool> StonyFromMap;
+
+        public readonly Dictionary<ThingDef, SettingHandle<bool>> Stuff = new Dictionary<ThingDef, SettingHandle<bool>>();
+
+        public IHaveMaterials()
+        {
+            Instance = this;
+        }
+
+        public override void DefsLoaded()
+        {
+            StonyFromMap = Settings.GetHandle<bool>(
+                "stonyFromMap",
+                "IHaveMaterials_stonyFromMap_title".Translate(),
+                "IHaveMaterials_stonyFromMap_desc".Translate(),
+                true
+            );
+
+            Stuff.Clear();
+
+            foreach (var thing in DefDatabase<ThingDef>.AllDefs)
+            {
+                if (!thing.IsStuff)
+                {
+                    continue;
+                }
+
+                Stuff.Add(thing, Settings.GetHandle<bool>(
+                    "stuff_" + thing.defName,
+                    "IHaveMaterials_stuff_title".Translate(thing.Named("THING")),
+                    "IHaveMaterials_stuff_desc".Translate(thing.description.Named("DESC"), (thing.modContentPack?.Name ?? "Unknown").Named("MOD")),
+                    thing.stuffProps.commonality >= 1
+                ));
+            }
+        }
+    }
+}
