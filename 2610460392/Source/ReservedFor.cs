0055867392f7e0dec3c6a2be9ffba975e8a18d41
Mlie <mlie@litet.net>
2023-02-02 13:01:00 +0100

    Reserved For

---
+++ 2610460392/Source/ReservedFor.cs
@@ -0,0 +1,30 @@
+﻿using HugsLib;
+using HugsLib.Settings;
+using Verse;
+
+namespace ReservedFor
+{
+    public class ReservedFor : ModBase
+    {
+        internal static ReservedFor Instance;
+
+        public override string ModIdentifier => "me.lubar.ReservedFor";
+
+        public ReservedFor()
+        {
+            Instance = this;
+        }
+
+        private SettingHandle<bool> showOtherFactions;
+
+        internal bool ShowOtherFactions => showOtherFactions;
+
+        public override void DefsLoaded()
+        {
+            showOtherFactions = Settings.GetHandle<bool>("showOtherFactions",
+                "ReservedFor_showOtherFactionReservations_title".Translate(),
+                "ReservedFor_showOtherFactionReservations_description".Translate(),
+                false);
+        }
+    }
+}
---
+++ 2610460392/Source/Thing_GetInspectStringLowPriority_Patch.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using HarmonyLib;
+using Verse;
+
+namespace ReservedFor
+{
+    [HarmonyPatch(typeof(Thing), nameof(Thing.GetInspectStringLowPriority), new Type[0])]
+    static class Thing_GetInspectStringLowPriority_Patch
+    {
+        static void Postfix(Thing __instance, ref string __result)
+        {
+            foreach (var reservation in __instance.Map.reservationManager.ReservationsReadOnly)
+            {
+                if (reservation.Target.Thing != __instance)
+                {
+                    continue;
+                }
+
+                if (!ReservedFor.Instance.ShowOtherFactions && !reservation.Faction.IsPlayer)
+                {
+                    continue;
+                }
+
+                var pawn = reservation.Claimant;
+                var job = reservation.Job.GetReport(pawn);
+
+                if (!string.IsNullOrEmpty(__result))
+                {
+                    __result += "\n";
+                }
+
+                if (reservation.StackCount == 0)
+                {
+                    __result += "ReservedFor_notReserved".Translate(pawn.Named("PAWN"), job.Named("JOB"));
+                }
+                else if (reservation.StackCount != -1 && reservation.Target.HasThing && reservation.Target.Thing.stackCount != reservation.StackCount)
+                {
+                    __result += "ReservedFor_reservedCount".Translate(reservation.StackCount.Named("COUNT"), pawn.Named("PAWN"), job.Named("JOB"));
+                }
+                else
+                {
+                    __result += "ReservedFor_reserved".Translate(pawn.Named("PAWN"), job.Named("JOB"));
+                }
+            }
+        }
+    }
+}
