682de3077b382ea79160cc8910264cca7f6d0370
Mlie <mlie@litet.net>
2023-02-02 14:01:47 +0100

    Rarr's Manhunting Rat Shells

---
+++ 2828121361/Source/Rarr/Building_TrapRat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using RimWorld;
+
+namespace Rarr
+{
+    public class Building_TrapRat : Building_Trap
+    {
+        private static readonly PawnKindDef rat = PawnKindDef.Named("Rat");
+        protected override void SpringSub(Pawn p)
+        {
+            Faction faction = FactionUtility.DefaultFactionFrom(rat.defaultFactionType);
+            Pawn pawn = GenSpawn.Spawn(PawnGenerator.GeneratePawn(rat, faction), base.Position, base.Map) as Pawn;
+            pawn.health.AddHediff(HediffDefOf.Scaria);
+            pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent);
+        }
+    }
+}
---
+++ 2828121361/Source/Rarr/Projectile_ManhuntingRatShell.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace Rarr
+{
+    public class Projectile_ManhuntingRatShell : Bullet
+    {
+		private static readonly PawnKindDef rat = PawnKindDef.Named("Rat");
+
+		protected override void Impact(Thing hitThing)
+		{
+			Faction faction = FactionUtility.DefaultFactionFrom(rat.defaultFactionType);
+			// Spawns 5 rats
+			for (int i = 0; i < 5; i++)
+            {
+				Pawn pawn = GenSpawn.Spawn(PawnGenerator.GeneratePawn(rat, faction), base.Position, base.Map) as Pawn;
+				pawn.health.AddHediff(HediffDefOf.Scaria);
+				pawn.mindState.mentalStateHandler.TryStartMentalState(MentalStateDefOf.ManhunterPermanent);
+			}
+			GenClamor.DoClamor(this, 2.1f, ClamorDefOf.Impact);
+			Destroy();
+		}
+	}
+}
