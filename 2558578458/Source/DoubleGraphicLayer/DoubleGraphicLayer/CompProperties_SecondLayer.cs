b87fddd3a0559fd094477d4a2db066898edf37eb
Mlie <mlie@litet.net>
2023-02-02 17:02:44 +0100

    Graphic Chair Overhaul - Update Version

---
+++ 2558578458/Source/DoubleGraphicLayer/DoubleGraphicLayer/CompProperties_SecondLayer.cs
@@ -0,0 +1,24 @@
+﻿using Verse;
+
+namespace DoubleGraphicLayer
+{
+    internal class CompProperties_SecondLayer : CompProperties
+    {
+        public GraphicData graphicData = null;
+
+        public AltitudeLayer altitudeLayer = AltitudeLayer.MoteOverhead;
+
+        public float Altitude
+        {
+            get
+            {
+                return Altitudes.AltitudeFor(this.altitudeLayer);
+            }
+        }
+
+        public CompProperties_SecondLayer()
+        {
+            this.compClass = typeof(CompSecondLayer);
+        }
+    }
+}
---
+++ 2558578458/Source/GCO_Settings/GCO_Settings/GCOSttngs.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections.Generic;
+using Verse;
+using UnityEngine;
+
+namespace MyExampleMod
+{
+    public class ExampleSettings : ModSettings
+    {
+        /// <summary>
+        /// The three settings our mod has.
+        /// </summary>
+        public bool exampleBool;
+        public float exampleFloat = 200f;
+        public List<Pawn> exampleListOfPawns = new List<Pawn>();
+
+        /// <summary>
+        /// The part that writes our settings to file. Note that saving is by ref.
+        /// </summary>
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref exampleBool, "exampleBool");
+            Scribe_Values.Look(ref exampleFloat, "exampleFloat", 200f);
+            Scribe_Collections.Look(ref exampleListOfPawns, "exampleListOfPawns", LookMode.Reference);
+            base.ExposeData();
+        }
+    }
+
+    public class ExampleMod : Mod
+    {
+        /// <summary>
+        /// A reference to our settings.
+        /// </summary>
+        ExampleSettings settings;
+
+        /// <summary>
+        /// A mandatory constructor which resolves the reference to our settings.
+        /// </summary>
+        /// <param name="content"></param>
+        public ExampleMod(ModContentPack content) : base(content)
+        {
+            this.settings = GetSettings<ExampleSettings>();
+        }
+
+        /// <summary>
+        /// The (optional) GUI part to set your settings.
+        /// </summary>
+        /// <param name="inRect">A Unity Rect with the size of the settins window.</param>
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.CheckboxLabeled("exampleBoolExplanation", ref settings.exampleBool, "exampleBoolToolTip");
+            listingStandard.Label("exampleFloatExplanation");
+            settings.exampleFloat = listingStandard.Slider(settings.exampleFloat, 100f, 300f);
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+        }
+
+        /// <summary>
+        /// Override SettingsCategory to show up in the list of settings.
+        /// Using .Translate() is optional, but does allow for localisation.
+        /// </summary>
+        /// <returns>The (translated) mod name.</returns>
+        public override string SettingsCategory()
+        {
+            return "MyExampleModName".Translate();
+        }
+    }
+}
