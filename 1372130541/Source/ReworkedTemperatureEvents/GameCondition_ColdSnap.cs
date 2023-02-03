79029750d312bdb109163708f2755a2638e2bf38
Mlie <mlie@litet.net>
2023-02-03 07:02:42 +0100

    Reworked Temperature Extreme Events [1.3]

---
+++ 1372130541/Source/ReworkedTemperatureEvents/GameCondition_ColdSnap.cs
@@ -0,0 +1,36 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace ReworkedTemperatureEvents
+{
+    public class GameCondition_ColdSnap : GameCondition
+    {
+        private static int seed = Find.TickManager.TicksGame;
+
+        private Random random = new Random(GameCondition_ColdSnap.seed);
+
+        private float MaxTempOffsetC = 0f;
+
+        private float MaxTempOffsetF = 0f;
+
+        private string DisplayTempOffsetC;
+
+        private string DisplayTempOffsetF;
+
+        private const int LerpTicks = 12000;
+
+        public override float TemperatureOffset()
+        {
+            if (this.MaxTempOffsetC == 0f)
+            {
+                this.MaxTempOffsetC = (float)this.random.Next(-23, -12);
+                this.MaxTempOffsetF = this.MaxTempOffsetC * 1.8f;
+                this.DisplayTempOffsetC = " " + this.MaxTempOffsetC.ToString() + "C";
+                this.DisplayTempOffsetF = " (" + this.MaxTempOffsetF.ToString() + "F)";
+                Log.Message("This cold snap resulted in the following temperature offset:" + this.DisplayTempOffsetC + this.DisplayTempOffsetF);
+            }
+            return GameConditionUtility.LerpInOutValue(this, 12000f, this.MaxTempOffsetC);
+        }
+    }
+}
---
+++ 1372130541/Source/ReworkedTemperatureEvents/GameCondition_HeatWave.cs
@@ -0,0 +1,37 @@
+﻿using RimWorld;
+using System;
+using Verse;
+
+namespace ReworkedTemperatureEvents
+{
+    public class GameCondition_HeatWave : GameCondition
+    {
+        private static int seed = Find.TickManager.TicksGame;
+
+        private Random random = new Random(GameCondition_HeatWave.seed);
+
+        private float MaxTempOffsetC = 0f;
+
+        private float MaxTempOffsetF = 0f;
+
+        private string DisplayTempOffsetC;
+
+        private string DisplayTempOffsetF;
+
+        private const int LerpTicks = 12000;
+
+        public override float TemperatureOffset()
+        {
+            bool flag = this.MaxTempOffsetC == 0f;
+            if (flag)
+            {
+                this.MaxTempOffsetC = (float)this.random.Next(10, 21);
+                this.MaxTempOffsetF = this.MaxTempOffsetC * 1.8f;
+                this.DisplayTempOffsetC = " " + this.MaxTempOffsetC.ToString() + "C";
+                this.DisplayTempOffsetF = " (" + this.MaxTempOffsetF.ToString() + "F)";
+                Log.Message("This heat wave resulted in the following temperature offset:" + this.DisplayTempOffsetC + this.DisplayTempOffsetF);
+            }
+            return GameConditionUtility.LerpInOutValue(this, 12000f, this.MaxTempOffsetC);
+        }
+    }
+}
