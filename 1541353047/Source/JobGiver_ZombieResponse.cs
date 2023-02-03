702a392c78274dcb8945798e845af3c90e0be0c8
Mlie <mlie@litet.net>
2023-02-03 05:02:36 +0100

    Zombiefied (Zombie Apocalypse)

---
+++ 1541353047/Source/JobGiver_ZombieResponse.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace Zombiefied
+{
+    // Token: 0x020000A4 RID: 164
+    public class JobGiver_ZombieResponse : ThinkNode_JobGiver
+    {
+        // Token: 0x060003E6 RID: 998 RVA: 0x0002923C File Offset: 0x0002763C
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            return this.TryGetAttackNearbyEnemyJob(pawn);
+        }
+
+        // Token: 0x060003E7 RID: 999 RVA: 0x000292AC File Offset: 0x000276AC
+        private Job TryGetAttackNearbyEnemyJob(Pawn pawn)
+        {
+            Pawn thing = BestPawnToHuntForPredator(pawn);
+            if (thing != null)
+            {
+                return new Job(ZombiefiedMod.zombieHunt, thing)
+                {
+                    killIncappedTarget = true,
+                    expiryInterval = (int)(Rand.RangeSeeded(1f, 2f, Find.TickManager.TicksAbs) * 700),
+                    attackDoorIfTargetLost = true
+                };
+            }
+            return null;
+        }
+
+        public Pawn BestPawnToHuntForPredator(Pawn predator, float range = 7f)
+        {
+            //List<Pawn> allPawnsSpawned = predator.Map.mapPawns.AllPawnsSpawned;
+            List<Thing> allThingsRegion = predator.GetRegion().ListerThings.AllThings;
+
+            Pawn pawnToReturn = null;
+            float num = 0f;
+
+            //for (int i = 0; i < allPawnsSpawned.Count; i++)
+            for (int i = 0; i < allThingsRegion.Count; i++)
+                {
+                Pawn pawn2 = allThingsRegion[i] as Pawn;
+                if (pawn2 != null && predator != pawn2)
+                {
+                    if (IsAcceptablePreyFor(predator, pawn2, range))
+                    {
+                        if (predator.CanReach(pawn2, PathEndMode.ClosestTouch, Danger.Deadly, false, false, TraverseMode.ByPawn))
+                        {
+                            if (!pawn2.IsForbidden(predator))
+                            {
+                                float preyScoreFor = GetPreyScoreFor(predator, pawn2);
+                                if (preyScoreFor > num || pawnToReturn == null)
+                                {
+                                    num = preyScoreFor;
+                                    pawnToReturn = pawn2;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+            return pawnToReturn;
+        }
+
+        public bool IsAcceptablePreyFor(Pawn predator, Pawn prey, float distance)
+        {
+            Pawn_Zombiefied preyZ = prey as Pawn_Zombiefied;
+            if (preyZ != null)
+            {
+                return false;
+            }         
+            if(ZombiefiedMod.disableZombiesAttackingAnimals && !prey.RaceProps.Humanlike)
+            {
+                return false;
+            }
+            if (!prey.RaceProps.IsFlesh)
+            {
+                return false;
+            }
+            float lengthHorizontal = -GetPreyScoreFor(predator, prey);
+            if (lengthHorizontal > distance)
+            {
+                return false;
+            }
+            
+            return true;
+        }
+
+        public float GetPreyScoreFor(Pawn predator, Pawn prey)
+        {
+            float lengthHorizontal = (predator.Position - prey.Position).LengthHorizontal;
+            return -lengthHorizontal;
+        }
+    }
+}
---
+++ 1541353047/Source/ZombiefiedPatches.cs
@@ -0,0 +1,122 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Reflection.Emit;
+using System.Text;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+using Verse.AI.Group;
+using Verse.Sound;
+
+namespace Zombiefied
+{
+    [StaticConstructorOnStartup]
+    class ZombiefiedPatches
+    {
+        static ZombiefiedPatches()
+        {
+            var harmony = new Harmony("com.github.harmony.rimworld.mod.zombiefied");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Pawn_MindState), nameof(Pawn_MindState.Notify_DamageTaken))]
+    class ManhunterPatch
+    {
+        static bool Prefix(Pawn_MindState __instance, ref DamageInfo dinfo)
+        {
+            if (__instance.pawn.AnimalOrWildMan() && dinfo.Instigator is Pawn_Zombiefied)
+            {
+                __instance.mentalStateHandler.Notify_DamageTaken(dinfo);
+                /*
+                if (dinfo.Def.ExternalViolenceFor(__instance.pawn))
+                {
+                    __instance.lastHarmTick = Find.TickManager.TicksGame;
+                    if (__instance.pawn.GetPosture() != PawnPosture.Standing)
+                    {
+                        __instance.lastDisturbanceTick = Find.TickManager.TicksGame;
+                    }
+                }
+                */
+                return false;
+            }
+            return true;
+        }
+    }
+
+
+    [HarmonyPatch(typeof(Verb), "CausesTimeSlowdown")]
+    class SlowTimePatch1
+    {
+        static bool Prefix(Verb __instance, ref LocalTargetInfo castTarg, ref bool __result)
+        {
+            Thing thing = castTarg.Thing;
+            if (thing != null && thing is Pawn_Zombiefied)
+            {
+                __result = false;
+                return false;
+            }  
+            return true;
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(FactionUtility), nameof(FactionUtility.HostileTo))]
+    class MechIgnoreZombs
+    {
+        static bool Prefix(ref Faction fac, ref Faction other, ref bool __result)
+        {
+            if ((fac != null && fac.def.defName.Equals("zombies")) || (other != null && other.def.defName.Equals("zombies")))
+            {
+                __result = false;
+                return false;
+            }
+            return true;
+        }
+    }
+    */
+
+    [HarmonyPatch(typeof(StatExtension), nameof(StatExtension.GetStatValue))]
+    class StatPatch
+    {
+        static bool Prefix(ref Thing thing, ref StatDef stat, ref float __result)
+        {
+            Pawn_Zombiefied zomb = thing as Pawn_Zombiefied; 
+            if (zomb != null)
+            {
+                if(stat.defName.Equals("ArmorRating_Sharp"))
+                {
+                    if(zomb.armorRating_Sharp > 0f)
+                    {
+                        __result = zomb.armorRating_Sharp;
+                        return false;
+                    }                    
+                }
+
+                if (stat.defName.Equals("ArmorRating_Blunt"))
+                {
+                    if (zomb.armorRating_Blunt > 0f)
+                    {
+                        __result = zomb.armorRating_Blunt;
+                        return false;
+                    }
+                }
+
+                if (stat.defName.Equals("ArmorRating_Heat"))
+                {
+                    if (zomb.armorRating_Heat > 0f)
+                    {
+                        __result = zomb.armorRating_Heat;
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
