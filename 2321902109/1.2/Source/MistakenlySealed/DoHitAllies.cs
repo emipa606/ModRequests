1254afc5500f77cf4e22b333c27cfb02fc956d8b
Mlie <mlie@litet.net>
2023-02-03 02:03:45 +0100

    Mistakenly sealed

---
+++ 2321902109/1.2/Source/MistakenlySealed/DoHitAllies.cs
@@ -0,0 +1,10 @@
+﻿using Verse;
+
+namespace MistS
+{
+    class DoHitAllies : DefModExtension
+    {
+        // 味方への誤射を禁止するか
+        public bool DontHitAllies = false;
+    }
+}
---
+++ 2321902109/1.2/Source/MistakenlySealed/Harmony.cs
@@ -0,0 +1,48 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace MistS
+{
+    [StaticConstructorOnStartup]
+    static class Patch_Core
+    {
+        static Patch_Core()
+        {
+            var harmony = new HarmonyLib.Harmony("bep.mistakenlysealed");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+
+	// 味方への誤射阻止
+	[HarmonyPatch(typeof(DamageWorker), "ExplosionDamageThing")]
+    [HarmonyPatch(new Type[] { typeof(Explosion), typeof(Thing), typeof(List<Thing>), typeof(List<Thing>), typeof(IntVec3) })]
+    internal static class Patches_EnemyKidnap
+	{
+		public static bool Prefix(Explosion explosion, Thing t, List<Thing> damagedThings, List<Thing> ignoredThings, IntVec3 cell, DamageWorker __instance)
+		{
+			if (__instance.def.GetModExtension<DoHitAllies>()?.DontHitAllies ?? false == true)
+            {
+                Pawn pawn = t as Pawn;
+                if (pawn != null)
+                {
+                    Thing attacker = explosion.instigator;
+                    if (attacker != null)
+                    {
+                        if (attacker.Faction != null && pawn.Faction != null)
+                        {
+                            if (attacker.Faction == pawn.Faction)
+                            {
+                                return false;
+                            }
+                        }
+                    }
+                }
+            }
+            return true;
+        }
+	}
+
+}
