20c46d5abfba9fd7acde871e9c96fa451a3f4a2d
Mlie <mlie@litet.net>
2023-02-03 10:03:32 +0100

    Desalination plant

---
+++ 2590011230/1.3/Source/DesalinationPlant/CompDesalinationPlant.cs
@@ -0,0 +1,42 @@
+﻿using DubsBadHygiene;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace DesalinationPlant
+{
+    public class CompProperties_DesalinationPlant : CompProperties
+    {
+        public float waterPerTick;
+        public CompProperties_DesalinationPlant()
+        {
+            this.compClass = typeof(CompDesalinationPlant);
+        }
+    }
+    public class CompDesalinationPlant : ThingComp
+    {
+        private CompPipe compPipe;
+
+        private CompPowerTrader compPower;
+        public CompProperties_DesalinationPlant Props => base.props as CompProperties_DesalinationPlant;
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            compPipe = this.parent.GetComp<CompPipe>();
+            compPower = this.parent.GetComp<CompPowerTrader>();
+        }
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (compPower.PowerOn)
+            {
+                compPipe.pipeNet.PushWater(Props.waterPerTick);
+            }
+        }
+    }
+}
---
+++ 2590011230/1.3/Source/DesalinationPlant/PlaceWorker_DesalinationPlant.cs
@@ -0,0 +1,51 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using Verse;
+
+namespace DesalinationPlant
+{
+	public class PlaceWorker_DesalinationPlant : PlaceWorker
+	{
+		public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot, Map map, Thing thingToIgnore = null, Thing thing = null)
+		{
+			var groundCells = GroundCells(loc, rot, out var waterCells);
+			if (waterCells.Any(x => !IsShallowOceanWater(x, map)))
+			{
+				return new AcceptanceReport("DP.MustBeOnShallowOceanWater".Translate());
+			}
+			else if (groundCells.Any(x => IsWater(x, Find.CurrentMap)))
+            {
+				return new AcceptanceReport("DP.MustBeOnNonWaterGround".Translate());
+			}
+			return true;
+		}
+		private bool IsWater(IntVec3 loc, Map map)
+		{
+			return map.terrainGrid.TerrainAt(loc).IsWater;
+		}
+		private bool IsShallowOceanWater(IntVec3 loc, Map map)
+		{
+			return map.terrainGrid.TerrainAt(loc) == TerrainDefOf.WaterOceanShallow;
+		}
+		public static List<IntVec3> GroundCells(IntVec3 loc, Rot4 rot, out List<IntVec3> waterCells)
+		{
+			var baseRect = CellRect.CenteredOn(loc, 5, 5);
+			var groundCells = baseRect.Cells.Where(x => !baseRect.GetEdgeCells(rot).Contains(x));
+			waterCells = baseRect.GetEdgeCells(rot).ToList();
+			return groundCells.ToList();
+		}
+		public override void DrawGhost(ThingDef def, IntVec3 loc, Rot4 rot, Color ghostCol, Thing thing = null)
+		{
+			var groundCells = GroundCells(loc, rot, out var waterCells);
+			Color color = (groundCells.Any(x => IsWater(x, Find.CurrentMap)) ? Designator_Place.CannotPlaceColor.ToOpaque() : Designator_Place.CanPlaceColor.ToOpaque());
+			GenDraw.DrawFieldEdges(groundCells.ToList(), color);
+			color = waterCells.Any(x => !IsShallowOceanWater(x, Find.CurrentMap)) ? Designator_Place.CannotPlaceColor.ToOpaque() : Designator_Place.CanPlaceColor.ToOpaque();
+			GenDraw.DrawFieldEdges(waterCells, color);
+		}
+    }
+}
