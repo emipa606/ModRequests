e89e350ce8d1d96dd3ade89f2c4b1988ba66d2f3
Mlie <mlie@litet.net>
2023-02-03 17:05:49 +0100

    Astoriel Facial Stuff

---
+++ 1380214417/Source/AlienBipedDrawer.cs
@@ -0,0 +1,15 @@
+﻿using AlienRace;
+using FacialStuff;
+using UnityEngine;
+using Verse;
+
+namespace AlienFaces
+{
+    public class AlienBipedDrawer : HumanBipedDrawer
+    {
+    public override void DrawAlienBodyAddons(bool portrait, Vector3 rootLoc, Quaternion quat, bool renderBody, Rot4 rotation)
+    {
+        HarmonyPatches.DrawAddons(portrait, this.Pawn, rootLoc, quat, rotation);
+    }
+    }
+}
