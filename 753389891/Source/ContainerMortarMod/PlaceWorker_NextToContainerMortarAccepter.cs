333c6a5dc5536c934a17185d150636ebb53c7ed1
Mlie <mlie@litet.net>
2023-02-03 23:05:12 +0100

    ContainerMortarMod

---
+++ 753389891/Source/ContainerMortarMod/PlaceWorker_NextToContainerMortarAccepter.cs
@@ -0,0 +1,33 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContainerMortarMod
+{
+    public class PlaceWorker_NextToContainerMortarAccepter : PlaceWorker
+    {
+
+        public override AcceptanceReport AllowsPlacing(BuildableDef checkingDef, IntVec3 loc, Rot4 rot)
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                IntVec3 c = loc + GenAdj.CardinalDirections[i];
+                if (c.InBounds())
+                {
+                    List<Thing> thingList = c.GetThingList();
+                    for (int j = 0; j < thingList.Count; j++)
+                    {
+                        Thing thing = thingList[j];
+                        if (thing.def.defName == "Turret_MortarContainer") {
+                            return true;
+                        }
+                    }
+                }
+            }
+            return "MustPlaceNextToContainerMortarAccepter".Translate();
+        }
+    }
+}
---
+++ 753389891/Source/ContainerMortarMod/Projectile_ContainerMortar.cs
@@ -0,0 +1,88 @@
+﻿using RimWorld;
+using Verse;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ContainerMortarMod
+{
+    public class Projectile_ContainerMortar : Projectile , IThingContainerOwner
+    {
+        private ThingContainer container = null;
+
+        public override void Tick()
+        {
+            if (this.ticksToImpact == this.StartingTicksToImpact) {
+                // first tick
+                if (this.launcher != null && this.launcher.def != null && this.launcher.def.defName == "Turret_MortarContainer") {
+                    LoadItem(this.launcher);
+                }
+            }
+            base.Tick();
+        }
+
+        public override void SpawnSetup()
+        {
+            base.SpawnSetup();
+            if (container == null)
+            {
+                container = new ThingContainer(this);
+            }
+        }
+
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Deep.LookDeep<ThingContainer>(ref container, "contains", new Object[] { this });
+        }
+
+        private void LoadItem(Thing launcher)
+        {
+            if (launcher.def.defName != "Turret_MortarContainer") return;
+
+            List<IntVec3> AdjCells = (from c in GenAdj.CellsAdjacentCardinal(launcher)
+                                      where c.InBounds()
+                                      select c).ToList<IntVec3>();
+            
+            for (int i = 0; i < AdjCells.Count; i++)
+            {
+                Thing thing = null;
+                Thing thing2 = null;
+                List<Thing> thingList = AdjCells[i].GetThingList();
+                for (int j = 0; j < thingList.Count; j++)
+                {
+                    Thing thing3 = thingList[j];
+                    if (thing3.IsInValidStorage())
+                    {
+                        thing = thing3;
+                    }
+                    if (thing3.def == ThingDef.Named("ContainerMortarLoadingStation"))
+                    {
+                        thing2 = thing3;
+                    }
+                }
+                if (thing != null && thing2 != null)
+                {
+                    container.TryAdd(thing);
+                }
+            }
+        }
+
+        public override void Destroy(DestroyMode mode = DestroyMode.Vanish)
+        {
+            base.Destroy(mode);
+            container.TryDropAll(this.Position, ThingPlaceMode.Near);
+        }
+
+        public ThingContainer GetContainer()
+        {
+            return container;
+        }
+
+        public IntVec3 GetPosition()
+        {
+            return this.Position;
+        }
+    }
+}
