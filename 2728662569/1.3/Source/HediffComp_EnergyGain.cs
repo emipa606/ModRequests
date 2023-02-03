68e3cd44ce5950bf0c477b892890789f3d381379
Mlie <mlie@litet.net>
2023-02-03 03:02:45 +0100

    Androids - Upgrade Tweaks

---
+++ 2728662569/1.3/Source/HediffComp_EnergyGain.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using RimWorld;
+using Verse;
+
+namespace AndroidsUpgradesTweaks
+{
+    class HediffComp_EnergyGain : HediffWithComps
+    {
+        List<Need> energy_needs;
+
+        bool initialized = false;
+
+        public override void Tick()
+        {
+            if (!initialized)
+            {
+                energy_needs = new List<Need>();
+                List<Need> needs = pawn.needs.AllNeeds;
+                for (int i = 0; i < needs.Count; i++)
+                {
+                    if (needs[i].def.defName == "ChJEnergy")
+                    {
+                        energy_needs.Add(needs[i]);
+                    }
+                }
+                initialized = true;
+            }
+
+            for (int i = 0; i < energy_needs.Count; i++)
+            {
+                energy_needs[i].CurLevel = energy_needs[i].MaxLevel;
+            }
+        }
+    }
+}
