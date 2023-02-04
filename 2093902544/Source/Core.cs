6993b5291f391588c6e09d1dc90d05ec2e19b496
Mlie <mlie@litet.net>
2023-02-04 05:04:59 +0100

    Custom Year and Speed

---
+++ 2093902544/Source/Core.cs
@@ -0,0 +1,185 @@
+﻿using UnityEngine;
+using Verse;
+using RimWorld;
+using HugsLib;
+using HugsLib.Settings;
+using System.Linq;
+
+
+namespace customYear
+{
+
+    public class modBase : ModBase
+    {
+        public override string ModIdentifier => "customYear";
+
+        private SettingHandle<int> startYear_s;
+        public static int startYear = 1;
+
+        private SettingHandle<int> unitYear_s;
+        public static int unitYear = 1;
+
+        private SettingHandle<int> humanAging_s;
+        public static int humanAging = 1;
+
+        private SettingHandle<int> lifeExpectancy_s;
+        public static int lifeExpectancy = 1;
+
+        private SettingHandle<int> animalAging_s;
+        public static int animalAging = 1;
+
+
+        private SettingHandle<bool> worldPawn_s;
+        public static bool worldPawn = false;
+
+
+        // -----------------------------------------
+
+        public override void DefsLoaded()
+        {
+            startYear_s = Settings.GetHandle<int>("startYear", "startYear".Translate(), "", 2100);
+            unitYear_s = Settings.GetHandle<int>("unitYear", "unitYear".Translate(), "", 10);
+
+            humanAging_s = Settings.GetHandle<int>("humanAging", "humanAging".Translate(), "", 10);
+            animalAging_s = Settings.GetHandle<int>("animalAging", "animalAging".Translate(), "", 3);
+
+            lifeExpectancy_s = Settings.GetHandle<int>("humanLifeExpectancy", "humanLifeExpectancy".Translate(), "", 80);
+
+            worldPawn_s = Settings.GetHandle<bool>("worldPawn", "worldPawn".Translate(), "", false);
+
+            SettingsChanged();
+        }
+
+        public override void SettingsChanged()
+        {
+
+            startYear = startYear_s.Value;
+            unitYear = unitYear_s.Value;
+            humanAging = Mathf.Clamp(humanAging_s.Value, 1, 10000);
+            lifeExpectancy = lifeExpectancy_s.Value;
+
+            animalAging = Mathf.Clamp(animalAging_s.Value, 1, 10000);
+
+            worldPawn = worldPawn_s.Value;
+
+            patchDef();
+        }
+
+        public static void patchDef()
+        {
+            /*
+            foreach (ThingDef t in from thing in DefDatabase<ThingDef>.AllDefs
+                                     where
+                                    thing.defName == "Human"
+                                   select thing)
+            {
+                t.race.lifeExpectancy =
+            }
+            */
+            ThingDefOf.Human.race.lifeExpectancy = lifeExpectancy;
+
+
+        }
+
+        
+        public override void Tick(int currentTick)
+        {
+            foreach(Pawn p in Find.WorldPawns.AllPawnsAlive)
+            {
+                if (p.IsColonist)
+                {
+                    customAging(p);
+                }
+            }
+            foreach (Map m in Find.Maps)
+            {
+                foreach (Pawn p in m.mapPawns.PawnsInFaction(Faction.OfPlayer))
+                {
+                    customAging(p);
+                }
+            }
+
+            // world pawn
+            if (worldPawn)
+            {
+                int checkTick = GenDate.TicksPerDay * 5;
+                bool flag = Find.TickManager.TicksAbs % checkTick == 0;
+                if (flag)
+                {
+
+                    RimWorld.Planet.WorldPawns worldPawns = Find.WorldPawns;
+                    Pawn[] ar_pawn = worldPawns.AllPawnsAlive.ToArray();
+
+                    foreach (Pawn p in ar_pawn)
+                    {
+                        if (!p.IsColonist)
+                        {
+                            int multiplier = 0;
+
+                            if (p.RaceProps.Humanlike)
+                            {
+                                multiplier = modBase.humanAging;
+                            }
+                            else
+                            {
+                                multiplier = modBase.animalAging;
+                            }
+
+                            int totalTick = checkTick * multiplier;
+
+                            p.ageTracker.AgeTickMothballed(totalTick);
+                        }
+                        
+
+                    }
+
+                }
+            }
+
+
+
+        }
+
+        void customAging(Pawn p)
+        {
+            int multiplier = 1;
+
+
+
+            if (p.RaceProps.Humanlike)
+            {
+                multiplier = modBase.humanAging;
+            }
+            else
+            {
+                multiplier = modBase.animalAging;
+            }
+
+            int additionalTick = multiplier - 1;
+
+            if (additionalTick <= 0) return;
+
+            //__instance.ageTracker.AgeTickMothballed(additionalTick);
+            p.TickMothballed(additionalTick);
+
+            //Log.Message($"--- {Find.TickManager.TicksAbs.ToString()}");
+
+            //Log.Message(p.ageTracker.AgeBiologicalTicks.ToString());
+        }
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+}
---
+++ 2093902544/Source/HarmonyPatches.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Reflection;
+using HarmonyLib;
+using Verse;
+using RimWorld;
+using UnityEngine;
+
+namespace customYear
+{
+
+    [StaticConstructorOnStartup]
+    public static class HarmonyPatches
+    {
+
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("customYear");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+
+        }
+
+    }
+
+    [HarmonyPatch(typeof(GenDate), "Year")]
+    [HarmonyPatch(new Type[] { typeof(long), typeof(float) })]
+    internal class patch_GenDate_Year
+    {
+        [HarmonyPrefix]
+        static bool Prefix(ref int __result, long absTicks, float longitude)
+        {
+            long num = absTicks + (long)GenDate.TimeZoneAt(longitude) * 2500L;
+            __result = modBase.startYear + Mathf.FloorToInt((float)num / 3600000f) * modBase.unitYear;
+            return false;
+        }
+    }
+
+    [HarmonyPatch(typeof(Verse.Pawn_AgeTracker), "AgeTickMothballed")]
+    public static class patch_Pawn_AgeTracker_AgeTickMothballed
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn_AgeTracker __instance)
+        {
+            if (Find.TickManager.TicksGame % GenDate.TicksPerDay == 0)
+            {
+                AccessTools.Method(__instance.GetType(), "RecalculateLifeStageIndex", null, null).Invoke(__instance, null);
+            }
+        }
+    }
+
+
+    /*
+    // pawn aging
+    [HarmonyPatch(typeof(Verse.Pawn), "Tick")]
+    public static class patch_pawn_tick
+    {
+        [HarmonyPostfix]
+        public static void Postfix(Pawn __instance)
+        {
+            if (!__instance.IsColonist) return;
+
+            int multiplier = 1;
+
+            
+
+            if (__instance.RaceProps.Humanlike)
+            {
+                multiplier = modBase.humanAging;
+            }
+            else
+            {
+                multiplier = modBase.animalAging;
+            }
+
+            int additionalTick = multiplier - 1;
+
+            if (additionalTick <= 0) return;
+
+            //__instance.ageTracker.AgeTickMothballed(additionalTick);
+            //__instance.TickMothballed(additionalTick);
+
+            Log.Message($"--- {Find.TickManager.TicksAbs.ToString()}");
+
+            Log.Message(__instance.ageTracker.AgeBiologicalTicks.ToString());
+
+
+        }
+    }
+    */
+
+
+
+
+}
