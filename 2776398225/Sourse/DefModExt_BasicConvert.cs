a1063e46faa72d8440511eb936968210be51b223
Mlie <mlie@litet.net>
2023-03-10 00:03:37 +0100

    Azuri Race Egglayer

---
+++ 2776398225/Sourse/DefModExt_BasicConvert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace O21Toolbox.PawnConversion
+{
+    public class DefModExt_BasicConvert : DefModExtension
+    {
+        public PawnKindDef defaultPawnKind;
+
+        public bool forceDropEquipment;
+        public bool killPawn;
+    }
+}
