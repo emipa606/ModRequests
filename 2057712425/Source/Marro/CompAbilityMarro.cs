7d6645ec92b6b79be017fd748a3734499ed30cbe
Mlie <mlie@litet.net>
2023-03-11 23:02:11 +0100

    Marro

---
+++ 2057712425/Source/Marro/CompAbilityMarro.cs
@@ -0,0 +1,38 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse.AI;
+using Verse;
+using AbilityUser;
+
+namespace Marro
+{
+    public class CompAbilityMarroWitch : CompAbilityUser
+    {
+        public override void CompTick()
+        {
+            base.CompTick();
+            if (IsInitialized)
+            {
+                // any custom code
+            }
+        }
+
+        public override void PostInitialize()
+        {
+            base.PostInitialize();
+
+            // add Abilities
+            this.AddPawnAbility(Marro.MyAbility);
+        }
+
+        public override bool TryTransformPawn()
+        {
+            // if true, transforms this pawn into this AbilityUser (i.e. initialize)
+            // So, you can have it check for a trait before initializing 
+            // by default, it returns true
+        }
+    }
+}
---
+++ 2057712425/Source/MarroLarvae/Class2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Verse;
+using RimWorld;
+
+namespace Marro
+{
+    // Token: 0x02000CE2 RID: 3298
+    public class CompProperties_MarroHatcher : CompProperties
+    {
+        // Token: 0x06004FCB RID: 20427 RVA: 0x001AA272 File Offset: 0x001A8472
+        public CompProperties_MarroHatcher()
+        {
+            this.compClass = typeof(CompMarroLarvae);
+        }
+
+        // Token: 0x04002C3B RID: 11323
+        public float hatcherDaystoHatch = 1f;
+
+        // Token: 0x04002C3C RID: 11324
+        public PawnKindDef hatcherPawn;
+    }
+}
