9be74c3946f2095d7d9abe027aab129cfd39ca9d
Mlie <mlie@litet.net>
2023-03-20 14:03:20 +0100

    Kenshi Cuisine

---
+++ 2600344770/Source/KenshiCuisine/Patches.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using UnityEngine;
+using Verse;
+
+namespace KenshiCuisine
+{
+    public class KenshiCuisineMod : Mod
+    {
+        private const string modId = "Super.Kenshicuisine";
+        Harmony Harmony { get; }
+        public KenshiCuisineMod(ModContentPack content) : base(content)
+        {
+            Harmony = new Harmony(modId);
+
+            var method = AccessTools.Method(typeof(Designator_ZoneAdd_Growing), nameof(Designator.CanDesignateCell), new Type[] { typeof(IntVec3) });
+            var patch = new HarmonyMethod(typeof(KenshiCuisineMod), nameof(Patch_Designator));
+            Harmony.Patch(method, postfix: patch);
+        }
+
+        static void Patch_Designator(IntVec3 c, Designator __instance, ref AcceptanceReport __result)
+        {
+            float num = ThingDefOf.Plant_Super_PricklyPear.plant.fertilityMin;
+            __result = __result || (__instance.Map.fertilityGrid.FertilityAt(c) >= num);
+        }
+    }
+}
---
+++ 2600344770/Source/KenshiCuisine/ThingDefOf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace KenshiCuisine
+{
+    public static class ThingDefOf
+    {
+        public static readonly ThingDef Plant_Super_PricklyPear = DefDatabase<ThingDef>.GetNamed("Plant_Super_PricklyPear");
+    }
+}
