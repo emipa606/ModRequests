0439f1ac13ad260a67cf00c71af700dfb5b73fba
Mlie <mlie@litet.net>
2023-12-24 09:05:38 +0100

    No more RiceWorld

---
+++ 2990082269/Source/NoMoreRiceWorld/NoMoreRiceWorld.cs
@@ -0,0 +1,34 @@
+﻿using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace NoMoreRiceWorld
+{
+
+    [HarmonyPatch(typeof(Toils_Ingest))]
+    [HarmonyPatch(nameof(Toils_Ingest.FinalizeIngest))]
+    static class ToilsIngestFinalizeIngestPatch
+    {
+        static void FinishAction()
+        {
+            Log.Message("Eating completed");
+        }
+
+        static void Postfix(ref Toil result)
+        {
+            result.AddFinishAction(FinishAction);
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    public static class NoMoreRiceWorld
+    {
+        static NoMoreRiceWorld()
+        {
+            var harmony = new Harmony("com.voidfirefly.nomorericeworld");
+            harmony.PatchAll();
+            Log.Message("Patching Eating");
+        }
+    }
+}
