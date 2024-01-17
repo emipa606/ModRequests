2a3ae49a8abb4d91a43bd13aeea65a7c670bb046
Mlie <mlie@litet.net>
2024-01-17 23:05:34 +0100

    [JAIL] Prison Walls

---
+++ 2566693272/Source/Building_PrisonBar.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI.Group;
+using Verse.Sound;
+
+namespace PrisonWalls
+{
+    public class Building_PrisonBar : Building
+    {
+        public override void TickRare()
+        {
+            Log.Message("Temperature changing");
+            GenTemperature.EqualizeTemperaturesThroughBuilding(this, 14f, twoWay: true);
+        }
+    }
+}
---
+++ 2566693272/Source/Harmony.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+using Verse.AI;
+using RimWorld;
+
+namespace PrisonWalls
+{
+    [StaticConstructorOnStartup]
+    public static class StartUp
+    {
+        static StartUp()
+        {
+            var harmony = new Harmony("com.example.patch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(RegionTypeUtility), "GetExpectedRegionType")]
+    class GetExpectedRegionTypePatch
+    {
+        public static void Postfix(ref RegionType __result, IntVec3 c, Map map)
+        {
+            if (__result != RegionType.None)
+            {
+                List<Thing> thingList = c.GetThingList(map);
+                for (int i = 0; i < thingList.Count; i++)
+                {
+                    if (thingList[i] is Building_PrisonBar)
+                    {
+                        __result = RegionType.None;
+                    }
+                }
+            }
+
+        }
+    }
+}
