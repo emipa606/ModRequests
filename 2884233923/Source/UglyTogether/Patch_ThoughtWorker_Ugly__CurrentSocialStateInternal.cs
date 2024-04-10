c81044b57f1fc6cc76c096232e36a292f08d9cbc
Mlie <mlie@litet.net>
2024-04-11 00:03:26 +0200

    Ugly Together

---
+++ 2884233923/Source/UglyTogether/Patch_ThoughtWorker_Ugly__CurrentSocialStateInternal.cs
@@ -0,0 +1,26 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using Verse;
+
+namespace UglyTogether
+{
+    [HarmonyPatch(typeof(ThoughtWorker_Ugly))]
+    [HarmonyPatch("CurrentSocialStateInternal", new[] { typeof(Pawn), typeof(Pawn) })]
+    public static class Patch_ThoughtWorker_Ugly__CurrentSocialStateInternal
+    {
+        public static void Postfix(ref ThoughtState __result, ref Pawn pawn)
+        {
+            if(!__result.Active)
+            {
+                return;
+            }
+
+            float statValue = pawn.GetStatValue(StatDefOf.PawnBeauty, true, -1);
+            if(statValue < 0f)
+            {
+                __result = ThoughtState.Inactive;
+            }
+        }
+    }
+}
---
+++ 2884233923/Source/UglyTogether/UglyTogether.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using UnityEngine;
+using RimWorld;
+using Verse;
+
+namespace UglyTogether
+{
+    public class UglyTogetherMod : Mod
+    {
+        public UglyTogetherMod(ModContentPack content) : base(content)
+        {
+            Log.Message("[UglyTogether]: Initialising Harmony");
+            new Harmony(this.Content.PackageIdPlayerFacing).PatchAll();
+        }
+    }
+}
