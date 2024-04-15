08fce56eeb92258c9fce6e84420eacaf42f888cd
Mlie <mlie@litet.net>
2024-04-15 23:03:16 +0200

    Consistent Map Stone

---
+++ 2654086748/1.3/Source/ConsistentMapStone/HarmonyPatches.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.Noise;
+
+namespace CMS
+{
+    [HarmonyPatch(typeof(RockNoises), "Init")]
+    class HarmonyPatches
+    {
+        static bool Prefix(Map map)
+		{
+			RockNoises.rockNoises = new List<RockNoises.RockNoise>();
+			foreach (ThingDef rockDef in Find.World.NaturalRockTypesIn(map.Tile))
+			{
+				RockNoises.RockNoise rockNoise = new RockNoises.RockNoise();
+				rockNoise.rockDef = rockDef;
+				//Use a consistent seed to make it reproducible, but add hashcode per rock type or else they all have the same noise in the end. The division by two is to make it less likely that we end up hitting the integer MaxValue.
+				rockNoise.noise = new Perlin(0.004999999888241291, 2.0, 0.5, 6, (map.ConstantRandSeed + rockDef.GetHashCode() / 2), QualityMode.Medium);
+				RockNoises.rockNoises.Add(rockNoise);
+				NoiseDebugUI.StoreNoiseRender(rockNoise.noise, rockNoise.rockDef + " score", map.Size.ToIntVec2);
+			}
+			return false;
+        }
+    }
+}
---
+++ 2654086748/1.3/Source/ConsistentMapStone/MapComponent_StoneGrid.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+
+namespace CMS
+{
+    public class MapComponent_StoneGrid : MapComponent
+    {
+        public ThingDef[] StoneGrid;
+
+        public MapComponent_StoneGrid(Map map) : base(map)
+        {
+
+        }
+
+        public ThingDef StoneChunkAt(IntVec3 c)
+        {
+            return StoneTypeAt(c).building.mineableThing;
+        }
+
+        public TerrainDef StoneTerrainAt(IntVec3 c)
+        {
+            return StoneTypeAt(c).building.naturalTerrain;
+        }
+
+        public ThingDef StoneTypeAt(IntVec3 c)
+        {
+            if (StoneGrid == null)
+                Initialize();
+            return StoneGrid[map.cellIndices.CellToIndex(c)];
+        }
+
+        public override void MapGenerated()
+        {
+            Initialize();
+        }
+
+        public void Initialize()
+        {
+            RockNoises.Init(map);
+            StoneGrid = new ThingDef[map.cellIndices.NumGridCells];
+            for (int i = 0; i < map.cellIndices.NumGridCells; i++)
+            {
+                var st = GenStep_RocksFromGrid.RockDefAt(map.cellIndices.IndexToCell(i));
+                StoneGrid[i] = st;
+            }
+            RockNoises.Reset();
+        }
+
+        public ThingDef ChunkFor(ThingDef rockDef)
+        {
+            Log.Message("Getting chunk name for " + rockDef.defName + ".");
+            return DefDatabase<ThingDef>.GetNamed(rockDef.defName + "Chunk");
+        }
+    }
+}
