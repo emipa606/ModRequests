4103174d3d4d1c536455f5771f8e6086be9977e1
Mlie <mlie@litet.net>
2024-04-15 17:09:04 +0200

    Xen's XP Fixer

---
+++ 947140943/Source/XenXP/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+using System.Reflection;
+
+namespace XenXP
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            var instance = new Harmony("rimworld.xen.exp");
+            instance.PatchAll(Assembly.GetExecutingAssembly());
+        }
+
+        [HarmonyPatch(typeof(SkillRecord))]
+        [HarmonyPatch("Interval")]
+        static class SkillRecord_Interval_Patch
+        {
+            static bool Prefix() => false;
+        }
+    }
+}
