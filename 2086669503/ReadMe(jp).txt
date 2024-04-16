dad13901b42f04b77c96a8f36c51d4ddb44bf7e6
Mlie <mlie@litet.net>
2024-04-16 10:05:24 +0200

    Appearance clothes - Humanoid Alien Races Patch[1.1-1.4]

---
+++ 2086669503/ReadMe(jp).txt
@@ -0,0 +1,33 @@
+【Appearance clothes - Alien Races Patch】
+・Appearance clothesとHumanoid Alien Races 2.0の連携MODです。
+　・Appearance clothes：https://steamcommunity.com/workshop/filedetails/?id=1537867047
+　・Humanoid Alien Races 2.0：https://steamcommunity.com/workshop/filedetails/?id=839005762
+・Humanoid Alien Races 2.0で追加された種族に見た目装備を設定する際、その種族が装着可能な装備しか設定できなくなります。
+
+
+■更新履歴
+[2022/11/13]1.4に対応
+・1.4に対応
+
+[2022/05/16]バグ修正
+・ヒトの見た目装備が設定できないバグを修正
+
+[2021/07/17]1.3に対応
+・1.3に対応
+
+[2020/08/14]1.2に対応
+・1.2に対応
+
+[2020/05/18]対応漏れしてた種族に対応
+・対応漏れしてた種族に対応
+
+[2020/05/06]公開
+
+
+■その他
+質問、不具合などがあれば、Twitter(@tammybee_tmb)か以下のページのコメント欄、RimMod相談所の#民火modにて報告をお願いします。
+
+・民火MOD | RimWorld私的wiki
+http://seesaawiki.jp/rimworld/d/%cc%b1%b2%d0%20MOD
+・RimMod相談所 - Discord
+https://discordapp.com/invite/WfVjNxs
---
+++ 2086669503/Source/HarmonyPatches.cs
@@ -0,0 +1,84 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+using AppearanceClothes;
+using AlienRace;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AppearanceClothes_AlienRacePatch {
+    [StaticConstructorOnStartup]
+    class HarmonyPatches {
+        static HarmonyPatches() {
+            var harmony = new Harmony("com.tammybee.apparentclothes.alienracepatch");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+	}
+
+	[HarmonyPatch(typeof(ITab_Pawn_AppearanceClothes))]
+	[HarmonyPatch("GetBodyTypeDefs")]
+	public static class ITab_Pawn_AppearanceClothes_GetBodyTypeDefs_Patch {
+		static void Postfix(Pawn pawn, ref List<BodyTypeDef> __result) {
+			ThingDef_AlienRace thingDef_AlienRace = pawn.def as ThingDef_AlienRace;
+            if (thingDef_AlienRace?.alienRace?.generalSettings?.alienPartGenerator != null && !thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.alienbodytypes.NullOrEmpty()) {
+				__result = thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.alienbodytypes;
+			}
+		}
+	}
+
+	[HarmonyPatch(typeof(ITab_Pawn_AppearanceClothes))]
+    [HarmonyPatch("CanAdd")]
+    public static class ITab_Pawn_AppearanceClothes_CanAdd_Patch {
+        static void Postfix(ITab_Pawn_AppearanceClothes __instance,ThingDef def, ref bool __result) {
+            if (__result) {
+                Pawn pawn = Traverse.Create(__instance).Property("SelPawnForGear").GetValue<Pawn>();
+                ThingDef_AlienRace thingDef_AlienRace = pawn.def as ThingDef_AlienRace;
+                if (thingDef_AlienRace != null) {
+                    __result = RaceRestrictionSettings.CanWear(def, pawn.def);
+                }
+            }
+        }
+    }
+
+    [HarmonyPatch(typeof(PawnGraphicSet_ResolveAllGraphics_Patch))]
+    [HarmonyPatch("GetNakedGraphic")]
+    public static class PawnGraphicSet_ResolveAllGraphics_Patch_GetNakedGraphic_Patch {
+        static void Postfix(Pawn pawn, BodyTypeDef bodyType, ref Graphic __result) {
+            ThingDef_AlienRace thingDef_AlienRace = pawn.def as ThingDef_AlienRace;
+            if (thingDef_AlienRace != null && pawn.def.defName != "Human") {
+                GraphicPaths currentGraphicPath = Traverse.Create(AccessTools.TypeByName("GraphicPathsExtension")).Method("GetCurrentGraphicPath").GetValue<GraphicPaths>(thingDef_AlienRace.alienRace.graphicPaths, pawn.ageTracker.CurLifeStage);
+                if (!currentGraphicPath.body.NullOrEmpty()) {
+                    AlienPartGenerator.AlienComp alienComp = pawn.GetComp<AlienPartGenerator.AlienComp>();
+                    string bodyMask = currentGraphicPath.bodyMasks.NullOrEmpty()
+                                          ? string.Empty
+                                          : currentGraphicPath.bodyMasks + ((alienComp.bodyMaskVariant >= 0
+                                                                           ? alienComp.bodyMaskVariant
+                                                                           : (alienComp.bodyMaskVariant =
+                                                                                  Rand.Range(min: 0, currentGraphicPath.BodyMaskCount))) > 0
+                                                                          ? alienComp.bodyMaskVariant.ToString()
+                                                                          : string.Empty);
+                    Shader shader = (ContentFinder<Texture2D>.Get(AlienPartGenerator.GetNakedPath(bodyType, currentGraphicPath.body, thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.useGenderedBodies ? pawn.gender.ToString() : "") + "_northm", false) == null) ? ShaderDatabase.Cutout : ShaderDatabase.CutoutComplex;
+                    __result = thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.GetNakedGraphic(bodyType, shader, pawn.story.SkinColor, thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.SkinColor(pawn, false), currentGraphicPath.body, pawn.gender.ToString(), bodyMask);
+                }
+            }
+        }
+    }
+
+    /*
+    [HarmonyPatch(typeof(PawnGraphicSet_ResolveAllGraphics_Patch))]
+    [HarmonyPatch("GetRottingGraphic")]
+    public static class PawnGraphicSet_ResolveAllGraphics_Patch_GetRottingGraphic_Patch {
+        static void Postfix(Pawn pawn, BodyTypeDef bodyType, ref Graphic __result) {
+            ThingDef_AlienRace thingDef_AlienRace = pawn.def as ThingDef_AlienRace;
+            if (thingDef_AlienRace != null) {
+                GraphicPaths currentGraphicPath = Traverse.Create(AccessTools.TypeByName("GraphicPathsExtension")).Method("GetCurrentGraphicPath").GetValue<GraphicPaths>(thingDef_AlienRace.alienRace.graphicPaths, pawn.ageTracker.CurLifeStage);
+                if (!currentGraphicPath.body.NullOrEmpty()) {
+                    __result = thingDef_AlienRace.alienRace.generalSettings.alienPartGenerator.GetNakedGraphic(bodyType, ShaderDatabase.Cutout, PawnGraphicSet.RottingColor, PawnGraphicSet.RottingColor, currentGraphicPath.body, pawn.gender.ToString());
+                }
+            }
+        }
+    }
+    */
+}
