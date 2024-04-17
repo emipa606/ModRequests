bc853683c1f6f01937ce2a303ceed729f733f0d2
Mlie <mlie@litet.net>
2024-04-17 17:06:14 +0200

    Doors Expanded

---
+++ 1316188771/Source/HeronDefOf.cs
@@ -0,0 +1,19 @@
+﻿using RimWorld;
+using Verse;
+
+namespace DoorsExpanded
+{
+    [DefOf]
+    public static class HeronDefOf
+    {
+        public static ThingDef HeronInvisibleDoor;
+
+        public static JobDef PH_UseRemoteButton;
+
+        public static StatDef DoorOpenTime;
+        public static StatDef PoweredDoorOpenTime;
+        public static StatDef UnpoweredDoorOpenTime;
+
+        static HeronDefOf() => DefOfHelper.EnsureInitializedInCtor(typeof(HeronDefOf));
+    }
+}
