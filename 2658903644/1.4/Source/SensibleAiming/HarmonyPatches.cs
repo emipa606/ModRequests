154f976df2cf4bd4a13979411b090ebadc39dc81
Mlie <mlie@litet.net>
2024-04-20 12:04:17 +0200

    Sensible Aiming

---
+++ 2658903644/1.4/Source/SensibleAiming/HarmonyPatches.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.Noise;
+using UnityEngine;
+
+namespace SA
+{
+	[HarmonyPatch(typeof(ShotReport), "AimOnTargetChance_IgnoringPosture", MethodType.Getter)]
+    class HarmonyPatches
+    {
+		static void Postfix(ref float __result)
+		{
+			__result *= 2;
+		}
+	}
+}
---
+++ 2658903644/1.4/Source/SensibleAiming/SensibleAiming.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace SA
+{
+    [StaticConstructorOnStartup]
+    public static class SensibleAiming
+    {
+        static SensibleAiming()
+        {
+            var harmony = new Harmony("UdderlyEvelyn.SensibleAiming");
+            harmony.PatchAll();
+        }
+    }
+}
