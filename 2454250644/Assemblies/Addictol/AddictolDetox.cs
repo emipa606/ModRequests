2d4b65efd63a1ff81c06d47db2f5e27b33f106a9
Mlie <mlie@litet.net>
2024-04-25 18:04:07 +0200

    Addictol [Abandoned]

---
+++ 2454250644/Assemblies/Addictol/AddictolDetox.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using Verse;
+
+namespace Addictol
+{
+    [DefOf]
+    class AddictolDefOf
+    {
+        public static HediffDef AddictolDetox;
+        static AddictolDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(AddictolDefOf));
+        }
+    }
+
+    public class IngestionOutcomeDoer_AddictolDetox : IngestionOutcomeDoer
+    {
+        /// Variables that control when the Addictol hediff is removed ///
+        public bool containsAddictol = false;
+        public float addHediffChance = 1f;
+        public HediffDef hediffToAdd;
+
+        /// Main function that executes the code upon ingestion ///
+        protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+        {
+            /// This section, along with the IF statement beneath it, are used to add the AddictolDetox Hediff ///
+            List<Hediff> hediffs1 = pawn.health.hediffSet.hediffs.ToList(); ;
+            foreach (Hediff hediff in hediffs1)
+            {
+                var StrHediff = hediff.ToString();
+
+                if (StrHediff.Contains("AddictolDetox"))
+                {
+                    containsAddictol = true;
+                    hediff.Severity += 1f;
+                }
+            }
+
+            /// Should Addictol not be active on the pawn ///
+            if (!containsAddictol)
+            {
+                pawn.health.AddHediff(AddictolDefOf.AddictolDetox);
+
+                List<Hediff> hediffs2 = pawn.health.hediffSet.hediffs.ToList(); ;
+                foreach (Hediff hediff in hediffs2)
+                {
+                    var StrHediff = hediff.ToString();
+
+                    if (StrHediff.Contains("AddictolDetox"))
+                    {
+                        hediff.Severity = 1f;
+                    }
+                }
+            }
+
+            /// Once Addictol has been added, this section iterates through all Hediffs on the pawn, removing all ones that Addictol cures ///
+            List<Hediff> hediffs3 = pawn.health.hediffSet.hediffs.ToList();
+            foreach (Hediff hediff in hediffs3)
+            {
+                /// This Switch iterates through all Hediffs, if any of the following cases are found, it removes it and then continues until the list is exhausted ///
+                switch (hediff.def.defName)
+                {
+                    case "AlcoholAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "AlcoholTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "AmbrosiaAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "AmbrosiaTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "PsychiteAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "PsychiteTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "SmokeleafAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "SmokeleafTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "WakeUpAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "WakeUpTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "GoJuiceAddiction":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    case "GoJuiceTolerance":
+                        pawn.health.RemoveHediff(hediff);
+                        break;
+                    default:
+                        System.Console.WriteLine("Nothing found");
+                        break;
+                }
+            }
+        }
+    }
+ }
---
+++ 2454250644/Assemblies/Addictol/IngestionOutcomeDoer_RemoveHediff.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+
+namespace RimWorld
+{
+	// Token: 0x020008BE RID: 2238
+	public class IngestionOutcomeDoer_RemoveHediff : IngestionOutcomeDoer
+	{
+		// Token: 0x060036BC RID: 14012 RVA: 0x0012A458 File Offset: 0x00128658
+		protected override void DoIngestionOutcomeSpecial(Pawn pawn, Thing ingested)
+		{
+			Hediff hediff = HediffMaker.MakeHediff(this.hediffDef, pawn, null);
+			float num;
+			if (this.severity > 0f)
+			{
+				num = this.severity;
+			}
+			else
+			{
+				num = this.hediffDef.initialSeverity;
+			}
+			if (this.divideByBodySize)
+			{
+				num /= pawn.BodySize;
+			}
+			AddictionUtility.ModifyChemicalEffectForToleranceAndBodySize(pawn, this.toleranceChemical, ref num);
+			hediff.Severity = num;
+			pawn.health.AddHediff(hediff, null, null, null);
+		}
+
+		// Token: 0x060036BD RID: 14013 RVA: 0x0012A4D2 File Offset: 0x001286D2
+		public override IEnumerable<StatDrawEntry> SpecialDisplayStats(ThingDef parentDef)
+		{
+			if (parentDef.IsDrug && this.chance >= 1f)
+			{
+				foreach (StatDrawEntry statDrawEntry in this.hediffDef.SpecialDisplayStats(StatRequest.ForEmpty()))
+				{
+					yield return statDrawEntry;
+				}
+				IEnumerator<StatDrawEntry> enumerator = null;
+			}
+			yield break;
+			yield break;
+		}
+
+		// Token: 0x04001DD0 RID: 7632
+		public HediffDef hediffDef;
+
+		// Token: 0x04001DD1 RID: 7633
+		public float severity = -1f;
+
+		// Token: 0x04001DD2 RID: 7634
+		public ChemicalDef toleranceChemical;
+
+		// Token: 0x04001DD3 RID: 7635
+		private bool divideByBodySize;
+	}
+}
