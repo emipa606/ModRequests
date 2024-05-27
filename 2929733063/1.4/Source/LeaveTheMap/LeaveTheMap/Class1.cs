b512aae97870f0977ecf40ebec4f76a85cab6a21
Mlie <mlie@litet.net>
2024-05-27 22:29:38 +0200

    Just Leave Already

---
+++ 2929733063/1.4/Source/LeaveTheMap/LeaveTheMap/Class1.cs
@@ -0,0 +1,32 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using Verse.Noise;
+
+namespace LeaveTheMap
+{
+    [StaticConstructorOnStartup]
+    public static class Class1
+    {
+        static Class1()
+        {
+            new Harmony("LeaveTheMap.Mod").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(ExitMapGrid), "get_MapUsesExitGrid")]
+    public static class ExitMapGrid_MapUsesExitGrid_Patch
+    {
+        public static void Postfix(Map ___map, ref bool __result)
+{
+            if (___map.IsPlayerHome)
+            {
+                __result = true;
+            }
+        }
+    }
+}
