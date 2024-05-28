87f7f11df58573adca7cccc4fd0b684118a3dc42
Mlie <mlie@litet.net>
2024-05-28 19:04:00 +0200

    Start With Any Fluid Meme

---
+++ 2610344106/Source/IdeoUtility_IsMemeAllowedForInitialFluidIdeo_Patch.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using HarmonyLib;
+using RimWorld;
+
+namespace StartWithAnyFluidMeme
+{
+    [HarmonyPatch(typeof(IdeoUtility), nameof(IdeoUtility.IsMemeAllowedForInitialFluidIdeo), new Type[] { typeof(MemeDef) })]
+    static class IdeoUtility_IsMemeAllowedForInitialFluidIdeo_Patch
+    {
+        static bool Prefix(ref bool __result, ref MemeDef memeDef)
+        {
+            __result = true;
+            return false;
+        }
+    }
+}
---
+++ 2610344106/Source/StartWithAnyFluidMeme.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+using Verse;
+
+namespace StartWithAnyFluidMeme
+{
+    [StaticConstructorOnStartup]
+    public static class StartWithAnyFluidMeme
+    {
+        static StartWithAnyFluidMeme()
+        {
+            var harmony = new Harmony("me.lubar.StartWithAnyFluidMeme");
+
+            harmony.PatchAll();
+        }
+    }
+}
