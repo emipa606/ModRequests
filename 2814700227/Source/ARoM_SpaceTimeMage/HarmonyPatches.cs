d26df49d08e42a2dd4ffd74ae25d32af0c8c1651
Mlie <mlie@litet.net>
2024-06-11 22:04:32 +0200

    A RimWorld of Magic - Space-Time Mage Class

---
+++ 2814700227/Source/ARoM_SpaceTimeMage/HarmonyPatches.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using HarmonyLib;
+using RimWorld;
+using TorannMagic;
+using Verse;
+using Verse.AI.Group;
+
+namespace ARoM_SpaceTimeMage
+{
+    [StaticConstructorOnStartup]
+    static partial class HarmonyPatches
+    {
+        private static readonly Type patchType = typeof(HarmonyPatches);
+
+        public static TraitDef TM_TimeFlayer = TraitDef.Named("TM_TimeFlayer");
+
+        private static Pawn currentInnerPawn = null;
+
+        static HarmonyPatches()
+        {
+            Harmony harmonyInstance = new Harmony("rimworld.NetzachSloth.ARoM_SpaceTimeMage");
+
+            harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Projectile_Resurrection), name: "ReduceSkillsOfPawn"),
+                    prefix: new HarmonyMethod(patchType, nameof(prefix_ReduceSkillsOfPawn_ARoM_SpaceTimeMage)));
+
+            harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Verb_ReverseTime), name: "AgeCorpse"),
+                    prefix: new HarmonyMethod(patchType, nameof(prefix_AgeCorpse_ARoM_SpaceTimeMage)));
+
+            harmonyInstance.Patch(original: AccessTools.Method(type: typeof(Verb_ReverseTime), name: "AgeCorpse"),
+                    postfix: new HarmonyMethod(patchType, nameof(postfix_AgeCorpse_ARoM_SpaceTimeMage)));
+        }
+
+        static bool prefix_ReduceSkillsOfPawn_ARoM_SpaceTimeMage(Verb_ReverseTime __instance, Pawn p)
+        {
+            if (p.story.traits.HasTrait(TM_TimeFlayer))
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+        }
+
+        static void prefix_AgeCorpse_ARoM_SpaceTimeMage(Verb_ReverseTime __instance, Thing thing)
+        {
+            currentInnerPawn = (thing as Corpse).InnerPawn;
+        }
+
+        static void postfix_AgeCorpse_ARoM_SpaceTimeMage(Verb_ReverseTime __instance)
+        {
+            Pawn CasterPawn = __instance.CasterPawn;
+
+            if (CasterPawn.story.traits.HasTrait(TM_TimeFlayer))
+            {
+                if (CasterPawn.health.hediffSet.HasHediff(TorannMagicDefOf.TM_DeathReversalHD))
+                {
+                    CasterPawn.health.RemoveHediff(CasterPawn.health.hediffSet.GetFirstHediffOfDef(TorannMagicDefOf.TM_DeathReversalHD));
+                }
+            }
+
+            if (currentInnerPawn.story.traits.HasTrait(TM_TimeFlayer))
+            {
+                if (currentInnerPawn.health.hediffSet.HasHediff(TorannMagicDefOf.TM_DeathReversalHD))
+                {
+                    currentInnerPawn.health.RemoveHediff(currentInnerPawn.health.hediffSet.GetFirstHediffOfDef(TorannMagicDefOf.TM_DeathReversalHD));
+                }
+            }
+
+            currentInnerPawn = null;
+        }
+
+    }
+}
---
+++ 2814700227/Source/ARoM_SpaceTimeMage/HediffComp_SpaceTimeMageHediff.cs
@@ -0,0 +1,50 @@
+﻿using RimWorld;
+using Verse;
+using System.Collections.Generic;
+using HarmonyLib;
+using System.Linq;
+
+namespace ARoM_SpaceTimeMage
+{
+    [StaticConstructorOnStartup]
+    internal class HediffComp_SpaceTimeMageHediff : HediffComp
+    {
+        private List<HediffDef> immuneHediffDefs = new List<HediffDef> { HediffDef.Named("TM_DeathReversalHD") };
+
+        private uint minuteTick = 3600;
+
+        public string labelCap
+        {
+            get
+            {
+                return base.Def.LabelCap;
+            }
+        }
+
+        public string label
+        {
+            get
+            {
+                return base.Def.label;
+            }
+        }
+
+
+        public override void CompPostTick(ref float severityAdjustment)
+        {
+            base.CompPostTick(ref severityAdjustment);
+
+            if (Find.TickManager.TicksGame % minuteTick == 0)
+            {
+                foreach (var immuneHediffDef in immuneHediffDefs)
+                {
+                    if (this.Pawn.health.hediffSet.HasHediff(immuneHediffDef))
+                    {
+                        this.Pawn.health.hediffSet.GetFirstHediffOfDef(immuneHediffDef).Severity = 0;
+                    }
+                }
+            }
+        }
+
+    }
+}
