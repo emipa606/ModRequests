d2c1c8509a9e55f80fcc335ffda768750be47a8a
Mlie <mlie@litet.net>
2024-06-21 08:03:48 +0200

    Explosion Buffs

---
+++ 3024235256/gayssa/gayssa/All.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+using System.Linq;
+
+namespace GayAss
+{
+    [DefOf]
+    public static class Slurs
+    {
+        public static HediffDef Crippled;
+
+        public static RecipeDef FixCripple;
+    }
+
+   [StaticConstructorOnStartup]
+    public class Patcher
+    {
+        static Patcher()
+        {
+            //DamageDefOf.Blunt.workerClass = typeof(BetterBlunt);
+
+            foreach (var def in DefDatabase<DamageDef>.AllDefs)
+            {
+                if (def.workerClass == typeof(Verse.DamageWorker_Blunt))
+                {
+                    def.workerClass = typeof(BetterBlunt);
+                }
+            }
+            ThingDefOf.Human.recipes.Add(Slurs.FixCripple);
+        }
+    }
+
+    public class BetterBlunt : DamageWorker_Blunt
+    {
+        BodyPartRecord record = null;
+
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            DamageResult result = null;
+            if (thing is Pawn p)
+            {
+                if (p.def == ThingDefOf.Human)
+                {
+                    record = ChooseHitPart(dinfo, p);
+
+                    if (record != null)
+                    {
+                        if (record?.def == BodyPartDefOf.Arm || record?.def == BodyPartDefOf.Leg)
+                        {
+                            var hp = p.health.hediffSet.GetPartHealth(record);
+                            bool flag = dinfo.Amount > hp;
+                            if (flag)
+                            {
+                                dinfo.SetAmount(Math.Max(hp - 1f, 0f));
+                                p.health.AddHediff(HediffMaker.MakeHediff(Slurs.Crippled, p, record));
+
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (result == null)
+            {
+                result = base.Apply(dinfo, thing);
+            }
+
+            return result;
+        }
+
+        protected override BodyPartRecord ChooseHitPart(DamageInfo dinfo, Pawn pawn)
+        {
+            if (record == null || (pawn?.health?.hediffSet?.HasHediff(Slurs.Crippled, record) ?? false))
+            {
+                record = base.ChooseHitPart(dinfo, pawn);
+            }
+            return record;
+        }
+    }
+}
---
+++ 3024235256/gayssa/gayssa/Class1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using Verse;
+using RimWorld;
+using System.Linq;
+
+namespace Bombastic
+{
+    [DefOf]
+    public class BaroDefOf
+    {
+        public static HediffDef BaroTrauma;
+    }
+
+
+    [StaticConstructorOnStartup]
+    public class Patcher
+    {
+        static Patcher()
+        {
+            //DamageDefOf.Blunt.workerClass = typeof(BetterBlunt);
+            DamageDefOf.Bomb.workerClass = typeof(BetterExplosion);
+        }
+    }
+
+    public class DamageDefExtension : DefModExtension
+    {
+        public List<BodyPartDef> partsOnWhichToBuff;
+
+        public float damageMult;
+    }
+
+    public class BetterExplosion : DamageWorker_Blunt
+    {
+        BodyPartRecord record = null;
+        public override DamageResult Apply(DamageInfo dinfo, Thing thing)
+        {
+            if (thing is Pawn p)
+            {
+                if (p.def == ThingDefOf.Human)
+                {
+                    record = ChooseHitPart(dinfo, p);
+                    var thingy = dinfo.Def.GetModExtension<DamageDefExtension>();
+                    if (thingy.partsOnWhichToBuff.Contains(record.def))
+                    {
+                        dinfo.SetAmount(dinfo.Amount * thingy.damageMult);
+                    }
+                }
+
+                if(record == null)
+                {
+                    record = ChooseHitPart(dinfo, p);
+                }
+
+                if (record.GetDirectChildParts().Any())
+                {
+                    foreach (var x in record.GetDirectChildParts().InRandomOrder())
+                    {
+                        float chance = 1f;
+                        if (Rand.Chance(chance) && x.depth == BodyPartDepth.Inside)
+                        {
+                            //Log.Message(x.def.label);
+
+                            var dinef = new DamageInfo(DamageDefOf.Crush, (dinfo.Amount / 2f), 0, -1, dinfo.Instigator, x, dinfo.Weapon);
+
+                            dinef.Def = DamageDefOf.Crush;
+
+                            //Log.Message((dinfo.Amount / 2f).ToString());
+
+                            dinef.SetAmount(dinfo.Amount / 2f);
+
+                            dinef.SetHitPart(x);
+
+                            p.TakeDamage(dinef);
+
+                            chance -= -0.15f;
+
+                            if (Rand.Chance(0.5f))
+                            {
+                                foreach (var child in x.GetDirectChildParts().Where(xy => xy.def.delicate && xy.def.IsSolid(xy, p.health.hediffSet.hediffs.Where(xz => xz.Part == xy).ToList())))
+                                {
+
+                                    var dinef2 = new DamageInfo(DamageDefOf.Crush, (dinfo.Amount / 2f), 0, -1, dinfo.Instigator, x, dinfo.Weapon);
+
+                                    dinef2.Def = DamageDefOf.Crush;
+
+                                    ////Log.Message((dinfo.Amount / 2f).ToString());
+
+                                    dinef2.SetAmount(dinfo.Amount / 4f);
+
+                                    dinef2.SetHitPart(x);
+
+                                    p.TakeDamage(dinef);
+                                }
+                            }
+                        }
+
+                    }
+                }
+            }
+            return base.Apply(dinfo, thing);
+        }
+
+    }
+}
