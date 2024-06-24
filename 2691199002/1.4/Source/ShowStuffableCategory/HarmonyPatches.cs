012053eff18b6fba0b185529f4a4bc3e96c9573f
Mlie <mlie@litet.net>
2024-06-24 19:04:34 +0200

    Show Stuff Categories

---
+++ 2691199002/1.4/Source/ShowStuffableCategory/HarmonyPatches.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace SSC
+{
+    [HarmonyPatch(typeof(Thing), "SpecialDisplayStats")]
+    public static class ShowStuffCategoriesPatch
+    {
+        static void Postfix(ref IEnumerable<StatDrawEntry> __result, Thing __instance)
+        {
+            if (!__instance.def.stuffCategories.EnumerableNullOrEmpty())
+            {
+                var stuffCategoryStrings = __instance.def.stuffCategories.Select(sc => sc.ToString()).ToArray();
+                string stringList = stuffCategoryStrings[0];
+                for (int i = 1; i < stuffCategoryStrings.Length; i++)
+                    stringList += ", " + stuffCategoryStrings[i];
+                Log.Message("Item has the following stuff categories in its def: " + stringList);
+                __result = __result.AddItem(new StatDrawEntry(StatCategoryDefOf.Basics, "Stuff categories used", stringList, "The categories of stuff used to make this.", 0));
+            }
+            if (__instance.def.stuffProps != null)
+            {
+                var stuffCategoryStrings = __instance.def.stuffProps.categories.Select(sc => sc.ToString()).ToArray();
+                string stringList = stuffCategoryStrings[0];
+                for (int i = 1; i < stuffCategoryStrings.Length; i++)
+                    stringList += ", " + stuffCategoryStrings[i];
+                __result = __result.AddItem(new StatDrawEntry(StatCategoryDefOf.Basics, "Stuff categories", stringList, "The categories of stuff that this falls under.", 0));
+            }
+        }
+    }
+}
---
+++ 2691199002/1.4/Source/ShowStuffableCategory/ShowStuffCategories.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RimWorld;
+using Verse;
+using HarmonyLib;
+
+namespace SSC
+{
+    [StaticConstructorOnStartup]
+    public static class ShowStuffCategories
+    {
+        static ShowStuffCategories()
+        {
+            var harmony = new Harmony("UdderlyEvelyn.ShowStuffCategories");
+            harmony.PatchAll();
+        }
+    }
+}
