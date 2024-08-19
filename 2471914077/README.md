103ce42038d5fd5eaee7b7de8965e4177dd246b6
Mlie <mlie@litet.net>
2024-08-19 20:11:40 +0200

    Remove Weather Overlays

---
+++ 2471914077/README.md
@@ -0,0 +1,5 @@
+# Remove Weather Overlays
+
+![](https://github.com/YipNZ/RemoveWeatherOverlays/blob/master/About/Preview.png)
+
+Removes Weather Overlays by patching the Verse.WeatherWorker.DrawWeather() method using Harmony. This should remove all weather related overlays without having to write XML Patches for all mods that add custom weather.
---
+++ 2471914077/Source/RemoveWeatherOverlays.cs
@@ -0,0 +1,41 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+
+namespace yipnz.RemoveWeatherOverlays
+{
+    [StaticConstructorOnStartup]
+    static class OverlayPatcher
+    {
+
+        static OverlayPatcher()
+        {
+            // Harmony.DEBUG = true;
+
+            Log.Message("RemoveWeatherOverlays - Patching Verse.WeatherWorker.DrawOverlays()");
+
+            var instance = new Harmony("yipnz.RWO");
+            
+            instance.PatchAll();
+        }
+
+    }
+
+    class DrawOverlayPatch
+    {
+        // skip autosave
+		[HarmonyPatch(typeof(WeatherWorker), nameof(WeatherWorker.DrawWeather))]
+		public class WeatherWorker_DrawWeather_Patch
+		{
+			public static bool Prefix()
+			{
+				return false;
+			}
+		}
+
+    }
+}
