b426e4c9c62d9cdf1d5a34e92a2108e59245bf24
Mlie <mlie@litet.net>
2024-08-30 08:04:36 +0200

    More Autosave Slots: Prevent accidental overwriting of autosaves [1.1 - 1.4]

---
+++ 2032167959/MoreAutosaveSlotsMod.cs
@@ -0,0 +1,113 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Reflection;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Verse;
+using Verse.Sound;
+
+namespace Revolus.MoreAutosaveSlots {
+    public class MoreAutosaveSlotsMod : Mod {
+        public static MoreAutosaveSlotsSettings Settings;
+
+        public MoreAutosaveSlotsMod(ModContentPack content) : base(content) {
+            Settings = this.GetSettings<MoreAutosaveSlotsSettings>();
+
+            var harmony = new Harmony(typeof(MoreAutosaveSlotsMod).AssemblyQualifiedName);
+            harmony.Patch(
+                AccessTools.Method(typeof(Autosaver), "AutosaverTick", new Type[] { }),
+                prefix: new HarmonyMethod(typeof(MoreAutosaveSlotsMod), nameof(Patch__Autosaver__AutosaverTick__Prefix))
+            );
+            harmony.Patch(
+                AccessTools.Method(typeof(Autosaver), "NewAutosaveFileName", new Type[] { }),
+                prefix: new HarmonyMethod(typeof(MoreAutosaveSlotsMod), nameof(Patch__Autosaver__NewAutosaveFileName__Prefix))
+            );
+            harmony.Patch(
+                AccessTools.Method(typeof(GenText), "IsValidFilename", new Type[] { typeof(string) }),
+                prefix: new HarmonyMethod(typeof(MoreAutosaveSlotsMod), nameof(Patch__GenText__IsValidFilename__Prefix))
+            );
+            harmony.Patch(
+                AccessTools.Constructor(typeof(Dialog_SaveFileList_Save), new Type[] { }),
+                postfix: new HarmonyMethod(typeof(MoreAutosaveSlotsMod), nameof(Patch__Dialog_SaveFileList_Save__Ctor__Postfix))
+            );
+        }
+
+        public override void DoSettingsWindowContents(Rect rect) {
+            bool changed;
+            
+            var listing = new Listing_Standard();
+            listing.Begin(rect);
+            try {
+                TextAnchor oldAnchorValue = Text.Anchor;
+                try {
+                    changed = Settings.ShowAndChangeSettings(listing);
+                } finally {
+                    Text.Anchor = oldAnchorValue;
+                }
+            } finally {
+                listing.End();
+            }
+
+            if (changed) {
+                SoundDefOf.DragSlider.PlayOneShotOnCamera();
+            }
+            
+            base.DoSettingsWindowContents(rect);
+        }
+
+        public override string SettingsCategory() {
+            return "MoreAutosaveSlots.SettingsName".Translate();
+        }
+
+        private static bool Patch__Autosaver__AutosaverTick__Prefix(ref Autosaver __instance) {
+            var hours = Settings.hours;
+            if (hours <= 0) {
+                return true;  // use default implementation
+            }
+
+            FieldInfo ticksSinceSaveField = AccessTools.Field(typeof(Autosaver), "ticksSinceSave");
+            if (ticksSinceSaveField is null) {
+                return true;  // use default implementation
+            }
+
+            Autosaver instance = __instance;
+
+            var ticksSinceSave = (int) ticksSinceSaveField.GetValue(instance);
+            var ticksPerDay = 60000;
+            var ticksPerHour = ticksPerDay / 24;
+            var doAutosaveThen = hours * ticksPerHour;
+
+            if (++ticksSinceSave >= doAutosaveThen) {
+                LongEventHandler.QueueLongEvent(instance.DoAutosave, "Autosaving", doAsynchronously: false, null);
+                ticksSinceSave = 0;
+            }
+            ticksSinceSaveField.SetValue(instance, ticksSinceSave);
+
+            return false;  // don't call default implementation
+        }
+
+        private static bool Patch__Autosaver__NewAutosaveFileName__Prefix(ref Autosaver __instance, ref string __result) {
+            __result = MoreAutosaveSlotsSettings.NextName();
+            return false;  // don't call default implementation
+        }
+
+        private static bool Patch__GenText__IsValidFilename__Prefix(ref bool __result, string str) {
+            __result = (str.Length <= 60) && !new Regex("[" + Regex.Escape(GenText.GetInvalidFilenameCharacters()) + "]").IsMatch(str);
+            return false;  // don't call default implementation
+        }
+
+        private static void Patch__Dialog_SaveFileList_Save__Ctor__Postfix(ref Dialog_SaveFileList_Save __instance) {
+            if (!Settings.useNextSaveName) {
+                return;
+            }
+
+            var typingName = AccessTools.Field(typeof(Dialog_SaveFileList_Save), "typingName");
+            if (typingName is null) {
+                return;
+            }
+
+            typingName.SetValue(__instance, MoreAutosaveSlotsSettings.NextName());
+        }
+    }
+}
---
+++ 2032167959/MoreAutosaveSlotsSettings.cs
@@ -0,0 +1,234 @@
+﻿using RimWorld;
+using RimWorld.Planet;
+using System;
+using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using Verse;
+
+namespace Revolus.MoreAutosaveSlots {
+    public class MoreAutosaveSlotsSettings : ModSettings {
+        private static readonly int countDefault = 5;
+        private static readonly int countMin = 1;
+        private static readonly int countMax = 60;
+        public int count = countDefault;
+
+        private static readonly string formatDefault = "{faction} ({index})";
+        public string format = formatDefault;
+
+        private static readonly int hoursDefault = 0;
+        private static readonly int hoursMin = 0;
+        private static readonly int hoursMax = 15 * 24;
+        public int hours = hoursDefault;
+
+        private static readonly bool useNextSaveNameDefault = true;
+        public bool useNextSaveName = useNextSaveNameDefault;
+
+        public override void ExposeData() {
+            Scribe_Values.Look(ref this.count, "count", countDefault);
+            Scribe_Values.Look(ref this.format, "format", formatDefault);
+            Scribe_Values.Look(ref this.hours, "hours", hoursDefault);
+            Scribe_Values.Look(ref this.useNextSaveName, "useNextSaveName", useNextSaveNameDefault);
+            base.ExposeData();
+        }
+
+        public static string[] AutoSaveNames(bool onlyOne = false) {
+            var settings = MoreAutosaveSlotsMod.Settings;
+            var count = Math.Max(Math.Min(settings.count, countMax), countMin);
+            var now = DateTime.Now;
+
+            string faction = null;
+            try { faction = Faction.OfPlayerSilentFail?.Name; } catch { }
+            if (string.IsNullOrEmpty(faction)) {
+                faction = "MoreAutosaveSlots.FactionNameDefault".Translate();
+            }
+
+            string settlement = null;
+            try { settlement = ((Settlement) Find.CurrentMap.info.parent).Name; } catch { }
+            if (string.IsNullOrEmpty(settlement)) {
+                settlement = "MoreAutosaveSlots.SettlementNameDefault".Translate();
+            }
+
+            var almostFormatted = (
+                settings.format.
+                Replace("{faction}", faction).
+                Replace("{settlement}", settlement).
+                Replace("{year}", now.Year.ToString("D4")).
+                Replace("{month}", now.Month.ToString("D2")).
+                Replace("{day}", now.Day.ToString("D2")).
+                Replace("{hour}", now.Hour.ToString("D2")).
+                Replace("{minute}", now.Minute.ToString("D2")).
+                Replace("{second}", now.Second.ToString("D2")).
+                Trim()
+            );
+
+            string doFormat(string s, int index) {
+                s = GenFile.SanitizedFileName(s.Replace("{index}", index.ToString()));
+                if (s.Length > 60) {
+                    s = s.Substring(0, 60).Trim();
+                }
+                return s;
+            }
+
+            if (onlyOne) {
+                return new string[] { doFormat(almostFormatted, count) };
+            }
+
+            var result = new string[count];
+            for (var i = 0; i < count; i++) {
+                result[i] = doFormat(almostFormatted, i + 1);
+            }
+            return result;
+        }
+
+        public static string NextName() {
+            var texts = AutoSaveNames();
+
+            var text = (from name in texts where !SaveGameFilesUtility.SavedGameNamedExists(name) select name).FirstOrDefault();
+            if (!(text is null)) {
+                return text;
+            }
+
+            return texts.MinBy((string name) => new FileInfo(GenFilePaths.FilePathForSavedGame(name)).LastWriteTime);
+        }
+
+        internal bool ShowAndChangeSettings(Listing_Standard listing) {
+            void line(string left, string right) {
+                Rect wholeRect = listing.GetRect(Text.LineHeight);
+
+                Text.Anchor = TextAnchor.MiddleRight;
+                Widgets.Label(wholeRect.LeftHalf().Rounded(), left);
+                
+                Text.Anchor = TextAnchor.MiddleLeft;
+                Widgets.Label(wholeRect.RightHalf().Rounded(), right);
+                
+                listing.Gap(listing.verticalSpacing);
+            }
+
+            int countNew, countOld = this.count;
+            string formatNew, formatOld = this.format;
+            int hoursNew, hoursOld = this.hours;
+            bool useNextSaveNameNew, useNextSaveNameOld = this.useNextSaveName;
+
+            var reset = Widgets.ButtonText(listing.GetRect(Text.LineHeight).RightHalf().LeftHalf().Rounded(), "MoreAutosaveSlots.ButtonReset".Translate());
+            listing.Gap(listing.verticalSpacing + Text.LineHeight);
+
+            line("MoreAutosaveSlots.LabelExample".Translate(), AutoSaveNames(true)[0]);
+            listing.Gap(Text.LineHeight);
+            
+            {
+                Rect wholeRect = listing.GetRect(2 * Text.LineHeight);
+                Rect labelRect = wholeRect.LeftHalf().Rounded();
+                Rect dataRect = wholeRect.RightHalf().Rounded();
+                Rect dataDescRect = dataRect.TopHalf().Rounded();
+                Rect dataSliderRect = dataRect.BottomHalf().Rounded();
+
+                Text.Anchor = TextAnchor.MiddleRight;
+                Widgets.Label(labelRect, "MoreAutosaveSlots.LabelCount".Translate());
+
+                Text.Anchor = TextAnchor.LowerCenter;
+                Widgets.Label(dataDescRect, countOld == 1 ? "MoreAutosaveSlots.CountOne".Translate() : "MoreAutosaveSlots.CountMultiple".Translate(countOld));
+                Text.Anchor = TextAnchor.UpperCenter;
+                countNew = Mathf.RoundToInt(Widgets.HorizontalSlider(dataSliderRect, countOld, countMin, countMax));
+
+                listing.Gap(listing.verticalSpacing + Text.LineHeight);
+            }
+            {
+                Rect wholeRect = listing.GetRect(Text.LineHeight);
+                Rect labelRect = wholeRect.LeftHalf().Rounded();
+                Rect dataRect = wholeRect.RightHalf().Rounded();
+
+                Text.Anchor = TextAnchor.MiddleRight;
+                Widgets.Label(labelRect, "MoreAutosaveSlots.LabelFormat".Translate());
+
+                Text.Anchor = TextAnchor.MiddleCenter;
+                formatNew = Widgets.TextArea(dataRect.Rounded(), formatOld);
+                listing.Gap(listing.verticalSpacing);
+
+                foreach (Match m in Regex.Matches("MoreAutosaveSlots.FormatDescription".Translate(), @"\|([^|]*)\|([^|]*)\|\|")) {
+                    var groups = m.Groups;
+                    line(groups[1].Value, groups[2].Value);
+                }
+                
+                listing.Gap(Text.LineHeight);
+            }
+            {
+                Rect wholeRect = listing.GetRect(2 * Text.LineHeight);
+                Rect labelRect = wholeRect.LeftHalf().Rounded();
+                Rect dataRect = wholeRect.RightHalf().Rounded();
+                Rect dataDescRect = dataRect.TopHalf().Rounded();
+                Rect dataSliderRect = dataRect.BottomHalf().Rounded();
+
+                string intervalDescription;
+                if (hoursOld <= 0) {
+                    intervalDescription = "MoreAutosaveSlots.HoursDisabled".Translate();
+                } else {
+                    var days = hoursOld / 24;
+                    var hours = hoursOld % 24;
+                    if (hours == 0) {
+                        if (days == 1) {
+                            intervalDescription = "MoreAutosaveSlots.HoursDaily".Translate();
+                        } else {
+                            intervalDescription = "MoreAutosaveSlots.HoursDays".Translate(days);
+                        }
+                    } else if (days == 0) {
+                        intervalDescription = "MoreAutosaveSlots.HoursHours".Translate(hours);
+                    } else {
+                        intervalDescription = "MoreAutosaveSlots.HoursDaysAndDays".Translate(days, hours);
+                    }
+                }
+
+                Text.Anchor = TextAnchor.MiddleRight;
+                Widgets.Label(labelRect, "MoreAutosaveSlots.LabelHours".Translate());
+
+                Text.Anchor = TextAnchor.LowerCenter;
+                Widgets.Label(dataDescRect, intervalDescription);
+                Text.Anchor = TextAnchor.UpperCenter;
+                hoursNew = Mathf.RoundToInt(Widgets.HorizontalSlider(dataSliderRect, hoursOld, hoursMin, hoursMax) / 3) * 3;
+                
+                listing.Gap(listing.verticalSpacing + Text.LineHeight);
+            }
+            {
+                Rect wholeRect = listing.GetRect(Text.LineHeight);
+                Rect labelRect = wholeRect.LeftHalf().Rounded();
+                Rect dataRect = wholeRect.RightHalf().Rounded();
+
+                Text.Anchor = TextAnchor.MiddleRight;
+                Widgets.Label(labelRect, "MoreAutosaveSlots.LabelUseNextSaveName".Translate());
+
+                Text.Anchor = TextAnchor.MiddleLeft;
+                useNextSaveNameNew = useNextSaveNameOld;
+                Widgets.CheckboxLabeled(dataRect, "", ref useNextSaveNameNew, placeCheckboxNearText: true);
+                
+                listing.Gap(listing.verticalSpacing);
+            }
+
+            if (reset) {
+                countNew = countDefault;
+                formatNew = formatDefault;
+                hoursNew = hoursDefault;
+                useNextSaveNameNew = useNextSaveNameDefault;
+            }
+
+            var changed = false;
+            if (countNew != countOld) {
+                this.count = countNew;
+                changed = true;
+            }
+            if (formatNew != formatOld) {
+                this.format = formatNew;
+                changed = true;
+            }
+            if (hoursNew != hoursOld) {
+                this.hours = hoursNew;
+                changed = true;
+            }
+            if (useNextSaveNameNew != useNextSaveNameOld) {
+                this.useNextSaveName = useNextSaveNameNew;
+                changed = true;
+            }
+            return changed;
+        }
+    }
+}
