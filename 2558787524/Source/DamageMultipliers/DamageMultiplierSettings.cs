e58f047cb257779b2fd7286ca94b7d35cd6a2f0a
Mlie <mlie@litet.net>
2024-09-02 21:12:04 +0200

    Damage Multipliers

---
+++ 2558787524/Source/DamageMultipliers/DamageMultiplierSettings.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HugsLib;
+using HugsLib.Settings;
+using Verse;
+
+namespace DamageMultipliers
+{
+    class DamageMultiplierSettings : ModBase
+    {
+        
+        public static float damageMultiplier;
+
+        public override string ModIdentifier
+        {
+            get { return "RimworldDamageMultiplier"; }
+        }
+
+        private SettingHandle<float> damageMultiplierSettingHandle;
+
+        public override void DefsLoaded()
+        {
+            damageMultiplierSettingHandle = Settings.GetHandle<float>("damageMultiplier", "Damage Multiplier", "Configure the multiplier applied to all damage.", 1.0f, Validators.FloatRangeValidator(0, 20));
+
+            updateSettingValues();
+        }
+
+        public override void SettingsChanged()
+        {
+            base.SettingsChanged();
+            updateSettingValues();
+        }
+
+        private void updateSettingValues()
+        {
+            damageMultiplier = damageMultiplierSettingHandle.Value;
+        }
+    }
+}
+
---
+++ 2558787524/Source/DamageMultipliers/ThingTakeDamagePatch.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace DamageMultipliers
+{
+    [HarmonyPatch(typeof(Verse.Thing))]
+    class ThingTakeDamagePatch
+    {
+        [HarmonyPatch("TakeDamage")]
+        //[HarmonyPatch(new Type[] { typeof(Verse.DamageInfo) })]
+        static void Prefix(ref DamageInfo dinfo)
+        {
+            //Verse.Log.Message("PATCH: " + dinfo.Amount + " to " + dinfo.Amount * DamageMultiplierSettings.damageMultiplier);
+            dinfo.SetAmount(dinfo.Amount * DamageMultiplierSettings.damageMultiplier);
+        }
+    }
+}
