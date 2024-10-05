0b20e74412c35d8dee0432094c7455190bb88a41
Mlie <mlie@litet.net>
2024-10-05 08:04:34 +0200

    RimHammer 40k - Contemptor Dreadnoughts

---
+++ 2952874126/Source/WarcasketStyles/StylesMod.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Emit;
+using UnityEngine;
+using Verse;
+using HarmonyLib;
+using System.Linq;
+using System;
+
+namespace WarcasketStyles
+{
+    public class StylesMod : Mod
+    {
+        public StylesMod(ModContentPack content) : base(content)
+        {
+            Harmony harmony = new Harmony("com.canonwarcasketstyles.patches");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+        }
+    }
+}
---
+++ 2952874126/Source/WarcasketStyles/WarcasketProject_Patches.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+using HarmonyLib;
+using RimWorld;
+using VFECore;
+using VFEPirates;
+
+namespace WarcasketStyles
+{
+
+    [HarmonyPatch(typeof(WarcasketProject), "ApplyOn")]
+    public class WarcasketProject_ApplyOn_Patch
+    {
+        public static void Postfix(ref Pawn pawn)
+        {
+            if (ModLister.IdeologyInstalled)
+            {
+                if (pawn.ideo == null)
+                {
+                    Log.Message("debug ideo is null for " + pawn.Name.ToStringShort);
+                    return;
+                }
+                List<Apparel> warcasketParts = pawn.apparel.WornApparel.Where(x => x is Apparel_Warcasket).ToList() ?? null;
+                
+                foreach (var casketPart in warcasketParts)
+                {
+                    if (casketPart.def.CanBeStyled())
+                    {
+                        ThingStyleDef style = pawn.Ideo.GetStyleCategoryFor(casketPart.def)?.GetStyleForThingDef(casketPart.def);
+                        if (style != null)
+                        {
+                            casketPart.StyleDef = style;
+                            pawn.apparel.Notify_ApparelChanged();
+                        }
+
+                    }
+                    
+                }
+            }
+
+        }
+    }
+}
