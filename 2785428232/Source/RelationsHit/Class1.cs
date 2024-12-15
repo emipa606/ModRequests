6cb5ff5b41fc910be25069d15b5a0108498527ef
Mlie <mlie@litet.net>
2024-12-15 08:04:26 +0100

    No Relations Hit from MemberDied

---
+++ 2785428232/Source/RelationsHit/Class1.cs
@@ -0,0 +1,29 @@
+﻿using Verse;
+using RimWorld;
+using HarmonyLib;
+
+namespace RelationsHit
+{
+    public class NoRelationsHit
+    {
+        [StaticConstructorOnStartup]
+        internal static class HarmonyInit
+        {
+            static HarmonyInit()
+            {
+                Harmony harmony = new Harmony("whatamidoing.relationsHit");
+
+                harmony.PatchAll();
+            }
+        }
+
+        [HarmonyPatch(typeof(Faction), "Notify_MemberDied")]
+        class RelationPatch
+        {
+            static void Prefix(ref bool wasWorldPawn)
+            {
+                wasWorldPawn = true;
+            }
+        }
+    }
+}
