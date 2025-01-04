376c26c880924f62074d5dfb31c74cdb74bac333
Mlie <mlie@litet.net>
2025-01-04 19:21:27 +0100

    Vanilla Skills Expanded - Vanilla Passions

---
+++ 3027879529/1.4/Source/VSENoPassionSystemMod.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Linq;
+using UnityEngine;
+using Verse;
+using VSE;
+using VSE.Passions;
+
+namespace VSENoPassionSystem
+{
+    public class VSENoPassionSystemMod : Mod
+    {
+        public VSENoPassionSystemMod(ModContentPack pack) : base(pack)
+        {
+			new Harmony("VSENoPassionSystemMod").PatchAll();
+        }
+    }
+
+    //[HarmonyPatch(typeof(PassionPatches), "Do")]
+    //public static class PassionPatches_Do_Patch
+    //{
+    //    public static bool Prefix()
+    //    {
+    //        return false;
+    //    }
+    //}
+
+    [HarmonyPatch(typeof(RandomPlusPatches), "Do")]
+    public static class RandomPlusPatches_Do_Patch
+    {
+        public static bool Prefix()
+        {
+            return false;
+        }
+    }
+}
