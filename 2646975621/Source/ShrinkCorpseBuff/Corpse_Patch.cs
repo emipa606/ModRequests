0d1f04cfe4251f74ce34745d63644c6336b59d2e
Mlie <mlie@litet.net>
2025-02-03 07:04:47 +0100

    Shrink Corpse Buff (Unnoficial v1.4)

---
+++ 2646975621/Source/ShrinkCorpseBuff/Corpse_Patch.cs
@@ -0,0 +1,67 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using Verse;
+
+namespace ShrinkCorpseBuff {
+    [StaticConstructorOnStartup]
+    [HarmonyPatch(typeof(Corpse))]
+    [HarmonyPatch("ButcherProducts")]
+    [HarmonyPatch(new Type[] { typeof(Pawn), typeof(float) })]
+    class Corpse_Patch {
+        /// <summary>
+        /// 初回起動でハーモニの起動を行います。
+        /// </summary>
+        static Corpse_Patch() {
+            var harmony = new Harmony("com.github.harmony.rimworld.mod.ShrinkCorpseBuff");
+            harmony.PatchAll(Assembly.GetExecutingAssembly());
+            results = new Dictionary<Corpse, IEnumerable<Thing>>();
+        }
+
+        /// <summary>
+        /// prefix から postfix にデータ移植するための保存用
+        /// </summary>
+        static Dictionary<Corpse, IEnumerable<Thing>> results;
+
+        static bool Prefix(Corpse __instance, Pawn butcher, ref float efficiency) {
+
+            var result = new List<Thing>();
+
+            var things = __instance.InnerPawn.ButcherProducts(butcher, efficiency);
+
+            foreach (var thing in things) {
+                result.Add(thing);
+            }
+
+            results[__instance] = result;
+
+            //Spread blood
+            if (__instance.InnerPawn.RaceProps.BloodDef != null) {
+                FilthMaker.TryMakeFilth(butcher.Position, butcher.Map, __instance.InnerPawn.RaceProps.BloodDef, __instance.InnerPawn.LabelIndefinite());
+            }
+            //Thought / tale for butchering humanlike
+            if (__instance.InnerPawn.RaceProps.Humanlike) {
+                butcher.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.ButcheredHumanlikeCorpse);
+                foreach (var p in butcher.Map.mapPawns.SpawnedPawnsInFaction(butcher.Faction)) {
+                    if (p == butcher || p.needs == null || p.needs.mood == null || p.needs.mood.thoughts == null
+                        /* 今回の追加分！ → */ || p.GetRoom() != __instance.GetRoom() /* 同じ部屋にいる場合のみ効果発動！ */
+                        ) {
+                        continue;
+                    }
+                    p.needs.mood.thoughts.memories.TryGainMemory(ThoughtDefOf.KnowButcheredHumanlikeCorpse);
+                }
+                TaleRecorder.RecordTale(TaleDefOf.ButcheredHumanlikeCorpse, butcher);
+            }
+            return false; // これで元のメソッドは実行されないのよね？
+        }
+
+        static void Postfix(Corpse __instance, ref IEnumerable<Thing> __result) {
+            if (results.ContainsKey(__instance)) {
+                __result = results[__instance];
+                results.Remove(__instance);
+            }
+        }
+    }
+}
---
+++ 2646975621/v1.4/Source/ShrinkCorpseBuff/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// アセンブリに関する一般情報は以下の属性セットをとおして制御されます。
+// アセンブリに関連付けられている情報を変更するには、
+// これらの属性値を変更してください。
+[assembly: AssemblyTitle("ShrinkCorpseBuff")]
+[assembly: AssemblyDescription("Shrink Corpse Buff on RimWorld.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Nekomimi-san wo MUKYOKA DE mofuru kai in Discord.")]
+[assembly: AssemblyProduct("Shrink Corpse Buff on Rimworld class library.")]
+[assembly: AssemblyCopyright("Copyright ©  2017 Fujisawa, Miyamiya")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// ComVisible を false に設定すると、このアセンブリ内の型は COM コンポーネントから
+// 参照できなくなります。COM からこのアセンブリ内の型にアクセスする必要がある場合は、
+// その型の ComVisible 属性を true に設定してください。
+[assembly: ComVisible(false)]
+
+// このプロジェクトが COM に公開される場合、次の GUID が typelib の ID になります
+[assembly: Guid("80BA5667-62C4-4F10-BC8E-5A96059E1C2B")]
+
+// アセンブリのバージョン情報は次の 4 つの値で構成されています:
+//
+//      メジャー バージョン
+//      マイナー バージョン
+//      ビルド番号
+//      Revision
+//
+// すべての値を指定するか、次を使用してビルド番号とリビジョン番号を既定に設定できます
+// 以下のように '*' を使用します:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
