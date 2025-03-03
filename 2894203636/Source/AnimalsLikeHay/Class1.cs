69e6ee7aa14a87e178262dc79057f390f04b3788
Mlie <mlie@litet.net>
2025-03-03 13:14:09 +0100

    Animals Like Hay

---
+++ 2894203636/Source/AnimalsLikeHay/Class1.cs
@@ -0,0 +1,39 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using RimWorld;
+using Verse;
+using Verse.AI;
+using System.Reflection.Emit;
+
+namespace AnimalsLikeHay
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Log.Message("JP_ALH: Loading..");
+            var harmony = new Harmony("AnimalsLikeHayPatch");
+            harmony.PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(ThingListGroupHelper))]
+    public static class ThingListGroupHelperPatch
+    {
+        [HarmonyPrefix]
+        [HarmonyPatch("Includes")]
+        public static void IncludesPatch(ref ThingRequestGroup group, ThingDef def)
+        {
+            if(group == ThingRequestGroup.FoodSourceNotPlantOrTree && def == ThingDefOf.Hay)
+            {
+                //Log.Message("Hay check");
+                group = ThingRequestGroup.FoodSource;
+            }
+        }
+    }
+
+}
