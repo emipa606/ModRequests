724de1e4536124ee783a561d1e2f2580562526bc
Mlie <mlie@litet.net>
2025-04-16 23:04:49 +0200

    Style Station Patch

---
+++ 2794438007/Source/StyleStationMoodPatch/StyleStationHarmonyPatch.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Reflection;
+using UnityEngine;
+using Verse;
+
+namespace StyleStationMoodPatch
+{
+    public class StyleStationHarmonyPatch
+    {
+        [StaticConstructorOnStartup]
+        public static class StyleStationHarmonyPatches
+        {
+            static StyleStationHarmonyPatches()
+            {
+
+                Harmony harmony = new Harmony("crazedmonkey231.stylestation.mood.patch");
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+
+            }
+        }
+
+        [HarmonyPatch(typeof(Pawn_StyleTracker), "SetupNextLookChangeData")]
+        [HarmonyPatch(new Type[] { typeof(HairDef), typeof(BeardDef), typeof(TattooDef), typeof(TattooDef), typeof(Color?) })]
+        static class FloatMenuMakerMap_ChoicesAtFor
+        {
+            static void Postfix(Pawn_StyleTracker __instance, HairDef hair, BeardDef beard, TattooDef faceTatoo, TattooDef bodyTattoo, Color? hairColor)
+            {
+
+                if (!StyleStationPatchMod.settings.changeHair)
+                {
+                    __instance.nextHairDef = null;
+                }
+
+                if (!StyleStationPatchMod.settings.changeBeard)
+                {
+                    __instance.nextBeardDef = __instance.beardDef;
+                }
+
+                if (!StyleStationPatchMod.settings.changeFaceTattoo)
+                {
+                    __instance.nextFaceTattooDef = __instance.FaceTattoo;
+                }
+
+                if (!StyleStationPatchMod.settings.changeBodyTattoo)
+                {
+                    __instance.nextBodyTatooDef = __instance.BodyTattoo;
+                }
+
+                if (!StyleStationPatchMod.settings.changeHairColor)
+                {
+                    __instance.nextHairColor = null;
+                }
+            }
+        }
+    }
+}
---
+++ 2794438007/Source/StyleStationMoodPatch/StyleStationSettings.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using Verse;
+
+namespace StyleStationMoodPatch
+{
+    public class StyleStationSettings : ModSettings
+    {
+
+        public bool changeHair = true;
+        public bool changeBeard = true;
+        public bool changeFaceTattoo = true;
+        public bool changeBodyTattoo = true;
+        public bool changeHairColor = true;
+
+        public override void ExposeData()
+        {
+            Scribe_Values.Look(ref changeHair, "changeHair");
+            Scribe_Values.Look(ref changeBeard, "changeBeard");
+            Scribe_Values.Look(ref changeFaceTattoo, "changeFaceTattoo");
+            Scribe_Values.Look(ref changeBodyTattoo, "changeBodyTattoo");
+            Scribe_Values.Look(ref changeHairColor, "changeHairColor");
+            base.ExposeData();
+        }
+    }
+
+    public class StyleStationPatchMod : Mod
+    {
+        public static StyleStationSettings settings;
+
+        public StyleStationPatchMod(ModContentPack content) : base(content)
+        {
+            StyleStationPatchMod.settings = this.GetSettings<StyleStationSettings>();
+        }
+
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(inRect);
+            listingStandard.Gap();
+            listingStandard.Label("-----Base Game Settings-----");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("Change Hair?", ref settings.changeHair, "Change Hair?");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("Change Beard?", ref settings.changeBeard, "Change Beard?");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("Change Face Tattoo?", ref settings.changeFaceTattoo, "Change Face Tattoo?");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("Change Body Tattoo?", ref settings.changeBodyTattoo, "Change Body Tattoo?");
+            listingStandard.Gap();
+            listingStandard.CheckboxLabeled("Change Hair Color?", ref settings.changeHairColor, "Change Hair Color?");
+            listingStandard.Gap();
+            listingStandard.End();
+            base.DoSettingsWindowContents(inRect);
+            StyleStationPatchMod.settings.Write();
+        }
+
+        public override string SettingsCategory()
+        {
+            return "Style Station Patch Settings";
+        }
+
+        public override void WriteSettings()
+        {
+
+            base.WriteSettings();
+        }
+    }
+}
