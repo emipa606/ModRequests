7573792c01b94da47648f34000a5244eafd0206b
Mlie <mlie@litet.net>
2025-07-13 18:08:26 +0200

    Fireplace

---
+++ 3236347705/Source/Fireplace/CompDarklightOverlay.cs
@@ -0,0 +1,52 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+
+namespace Fireplace
+{
+    [StaticConstructorOnStartup]
+    public class CompDarklightOverlay : CompFireOverlayBase
+    {
+        public new CompProperties_DarklightOverlay Props
+        {
+            get
+            {
+                return (CompProperties_DarklightOverlay)props;
+            }
+        }
+
+        public override void PostDraw()
+        {
+            base.PostDraw();
+            if (refuelableComp != null && !refuelableComp.HasFuel)
+            {
+                return;
+            }
+            Vector3 drawPos = parent.DrawPos;
+            drawPos.y += 0.03846154f;
+            DarklightGraphic.Draw(drawPos, parent.Rotation, parent, 0f);
+        }
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+            refuelableComp = parent.GetComp<CompRefuelable>();
+        }
+
+        public override void CompTick()
+        {
+            if (refuelableComp != null && !refuelableComp.HasFuel)
+            {
+                return;
+            }
+            if (startedGrowingAtTick < 0)
+            {
+                startedGrowingAtTick = GenTicks.TicksAbs;
+            }
+        }
+
+        protected CompRefuelable refuelableComp;
+
+        public static readonly Graphic DarklightGraphic = GraphicDatabase.Get<Graphic_Flicker>("Things/Special/Darklight", ShaderDatabase.TransparentPostLight, Vector2.one, Color.white);
+    }
+}
---
+++ 3236347705/Source/Fireplace/CompProperties_DarklightOverlay.cs
@@ -0,0 +1,26 @@
+﻿using RimWorld;
+using Verse;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fireplace
+{
+    public class CompProperties_DarklightOverlay : CompProperties_FireOverlay
+    {
+        public CompProperties_DarklightOverlay()
+        {
+            compClass = typeof(Fireplace.CompDarklightOverlay);
+        }
+
+        public override void DrawGhost(IntVec3 center, Rot4 rot, ThingDef thingDef, Color ghostCol, AltitudeLayer drawAltitude, Thing thing = null)
+        {
+            Graphic graphic = GhostUtility.GhostGraphicFor(Fireplace.CompDarklightOverlay.DarklightGraphic, thingDef, ghostCol, null);
+            Vector3 loc = center.ToVector3ShiftedWithAltitude(drawAltitude) + thingDef.graphicData.DrawOffsetForRot(rot) + DrawOffsetForRot(rot);
+            graphic.DrawFromDef(loc, rot, thingDef, 0f);
+        }
+    }
+}
