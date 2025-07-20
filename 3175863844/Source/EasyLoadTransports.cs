dfbf3f3a645650fe5b4918f103b077496767b066
Mlie <mlie@litet.net>
2025-07-20 18:08:22 +0200

    Easy Load Transports

---
+++ 3175863844/Source/EasyLoadTransports.cs
@@ -0,0 +1,35 @@
+﻿namespace EasyLoadTransports
+{
+    using HarmonyLib;
+    using RimWorld;
+    using Verse;
+
+    using System;
+
+    [StaticConstructorOnStartup]
+
+    public class EasyLoadTransports : Mod
+    {
+        public EasyLoadTransports(ModContentPack pack) : base(pack)
+        {
+            new Harmony("rimworld.scott2521.EasyLoadTransports").PatchAll();
+        }
+    }
+
+    [HarmonyPatch(typeof(Dialog_LoadTransporters), "TryAccept")]
+    public static class Dialog_LoadTransporters_TryAccept_Patch
+    {
+        public static bool Prefix(Dialog_LoadTransporters __instance)
+        {
+            if (GenHostility.AnyHostileActiveThreatToPlayer(__instance.map) is false)
+            {
+                if (__instance.DebugTryLoadInstantly())
+                {
+                    __instance.Close();
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
