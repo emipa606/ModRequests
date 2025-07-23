42db248ec353d316bc4b22699048e8ae8e31710a
Mlie <mlie@litet.net>
2025-07-24 00:12:24 +0200

    Save Our Ship 2 - Rimatomics Compatibility Patch

---
+++ 1910684003/Source/RimworldMod/Building_ShipCapacitorPPC.cs
@@ -0,0 +1,41 @@
+﻿using Rimatomics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace RimWorld
+{
+    [StaticConstructorOnStartup]
+    class Building_ShipCapacitorPPC : Building_PPC
+    {
+        public override void Tick()
+        {
+            base.Tick();
+            this.BarTo = 0;
+        }
+
+        private static Graphic barGraphic = GraphicDatabase.Get(typeof(Graphic_Multi), "Things/Building/Ship/CapacitorBar", ShaderDatabase.Cutout, new Vector2(3, 5), Color.white, Color.white);
+
+        public override void Draw()
+        {
+            base.Draw();
+            Color barColor;
+            if (this.TryGetComp<CompPowerBattery>().StoredEnergyPct < 0.25f)
+                barColor = new Color(0.25f + this.TryGetComp<CompPowerBattery>().StoredEnergyPct * 3, 0, 0);
+            else
+            {
+                float angle = (this.TryGetComp<CompPowerBattery>().StoredEnergyPct - 0.25f) * 2 * Mathf.PI / 3;
+                barColor = new Color(Mathf.Cos(angle), Mathf.Sin(angle), 0);
+            }
+            barGraphic.GetColoredVersion(ShaderDatabase.Cutout, barColor, barColor).Draw(new Vector3(this.DrawPos.x, this.DrawPos.y + 1f, this.DrawPos.z), this.Rotation, this);
+        }
+
+        public override void SpawnSetup(Map map, bool respawningAfterLoad)
+        {
+            base.SpawnSetup(map, respawningAfterLoad);
+        }
+    }
+}
---
+++ 1910684003/Source/RimworldMod/SoSRimatomicPatch.cs
@@ -0,0 +1,75 @@
+﻿using Harmony;
+using HugsLib;
+using Rimatomics;
+using RimWorld.Planet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using UnityEngine;
+using Verse;
+
+namespace RimworldMod
+{
+    public class SoSRimatomicPatch : ModBase
+    {
+        public override string ModIdentifier
+        {
+            get
+            {
+                return "SoSRimatomicsPatch";
+            }
+        }
+
+        public SoSRimatomicPatch()
+        {
+        }
+    }
+
+    [HarmonyPatch(typeof(Building_Railgun))]
+    [HarmonyPatch("WorldRange", MethodType.Getter)]
+    public class RailgunsInSpace
+    {
+        [HarmonyPostfix]
+        public static void InfiniteRange(Building_Railgun __instance, ref int __result)
+        {
+            if (__instance.Map != null && __instance.Map.Biome.defName.Equals("OuterSpaceBiome"))
+                __result = 99999;
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldObject_Sabot))]
+    [HarmonyPatch("Start", MethodType.Getter)]
+    public static class FromSpaceship
+    {
+        [HarmonyPostfix]
+        public static void StartAtShip(WorldObject_Sabot __instance, ref Vector3 __result)
+        {
+            int initialTile = (int)typeof(WorldObject_Sabot).GetField("initialTile", BindingFlags.Instance | BindingFlags.NonPublic).GetValue(__instance);
+            foreach (WorldObject ship in Find.World.worldObjects.AllWorldObjects.Where(o => o.def.defName.Equals("ShipOrbiting")))
+                if (ship.Tile == initialTile)
+                    __result = ship.DrawPos;
+            foreach (WorldObject site in Find.World.worldObjects.AllWorldObjects.Where(o => o.def.defName.Equals("SiteSpace")))
+                if (site.Tile == initialTile)
+                    __result = site.DrawPos;
+        }
+    }
+
+    [HarmonyPatch(typeof(WorldObject_Sabot))]
+    [HarmonyPatch("End", MethodType.Getter)]
+    public static class ToSpaceship
+    {
+        [HarmonyPostfix]
+        public static void EndAtShip(WorldObject_Sabot __instance, ref Vector3 __result)
+        {
+            int destTile = __instance.destinationTile;
+            foreach (WorldObject ship in Find.World.worldObjects.AllWorldObjects.Where(o => o.def.defName.Equals("ShipOrbiting")))
+                if (ship.Tile == destTile)
+                    __result = ship.DrawPos;
+            foreach (WorldObject site in Find.World.worldObjects.AllWorldObjects.Where(o => o.def.defName.Equals("SiteSpace")))
+                if (site.Tile == destTile)
+                    __result = site.DrawPos;
+        }
+    }
+}
