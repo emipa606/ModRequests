7d98c0f83243401d909f52b6564458c64cccce3f
Mlie <mlie@litet.net>
2025-08-12 23:10:57 +0200

    Log After Def Error

---
+++ 3291238448/Source/LogAfterDefError/Harmony.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using UnityEngine;
+using Verse;
+using RimWorld;
+
+namespace LogAfterDefError {
+
+	internal static class HarmonyPatches {
+		internal static Harmony Instance { get; private set; }
+
+		static HarmonyPatches() {
+			Instance = new Harmony("ordpus.logafterdeferror");
+			Instance.PatchAll(Assembly.GetExecutingAssembly());
+		}
+
+	}
+
+
+	[HarmonyPatch(typeof(DirectXmlLoader), nameof(DirectXmlLoader.DefFromNode))]
+	internal static class Verse__DirectXmlLoader__DefFromNode {
+
+		internal static void Prefix(ref LogMessage __state) {
+			__state = Log.messageQueue.lastMessage;
+		}
+
+		internal static void Postfix(Def __result, LoadableXmlAsset loadingAsset, LogMessage __state) {
+			if(__result == null || !LogAfterDefErrorModSettings.enabled || Log.messageQueue.lastMessage == __state) return;
+			var mod = loadingAsset?.mod ?? null;
+			if(Log.messageCount > 900) Log.ResetMessageCount();
+			Log.Message("[Def Error] " + __result.defName + ":" + mod?.Name + loadingAsset?.FullFilePath?.Replace(mod?.RootDir, "") + "\n");
+		}
+	}
+
+}
---
+++ 3291238448/Source/LogAfterDefError/LogAfterDefError.cs
@@ -0,0 +1,38 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using Verse;
+
+namespace LogAfterDefError {
+
+	public class LogAfterDefErrorMod : Mod {
+
+		public LogAfterDefErrorMod(ModContentPack content) : base(content) {
+			GetSettings<LogAfterDefErrorModSettings>();
+			RuntimeHelpers.RunClassConstructor(typeof(HarmonyPatches).TypeHandle);
+		}
+
+		public override void DoSettingsWindowContents(Rect inRect) {
+			var listing = new Listing_Standard();
+			listing.Begin(inRect);
+			listing.CheckboxLabeled("LogAfterDefError.Settings.Enabled".Translate(), ref LogAfterDefErrorModSettings.enabled);
+			listing.End();
+		}
+
+		public override string SettingsCategory() {
+			return "LogAfterDefError.Settings".Translate();
+		}
+	}
+
+	public class LogAfterDefErrorModSettings : ModSettings {
+
+		public static bool enabled = true;
+
+		public override void ExposeData() {
+			Scribe_Values.Look(ref enabled, "enabled", true);
+		}
+
+	}
+
+}
