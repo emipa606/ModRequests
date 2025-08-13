81890b8cce51470ef3fba79180140ade5f529e5e
Mlie <mlie@litet.net>
2025-08-13 12:08:47 +0200

    No More Lethal Damage Threshold

---
+++ 2657551690/Source/NMLDT/NMLDT/HarmonyPatches.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using Verse;
+
+namespace NMLDT
+{
+    [StaticConstructorOnStartup]
+    static class HarmonyPatches
+    {
+        static HarmonyPatches()
+        {
+            Harmony harmony = new Harmony("Jdalt.RimWorld.NMLDT.Main");
+            harmony.Patch(typeof(Pawn_HealthTracker).GetMethod("ShouldBeDeadFromLethalDamageThreshold"), null, new HarmonyMethod(typeof(HarmonyPatches), nameof(LDTCorrection)));
+        }
+        public static void LDTCorrection(ref bool __result)
+        {
+            __result = false;
+            return;
+        }
+    }
+}
