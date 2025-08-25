4637a8922f1a3972007aba9745b44b0c1719e18c
Mlie <mlie@litet.net>
2025-08-25 23:23:51 +0200

    Advanced Band Nodes

---
+++ 2886516228/Defs/AdvancedBandNodeDefOf.cs
@@ -0,0 +1,16 @@
+﻿using RimWorld;
+using Verse;
+
+namespace Hydroxyapatite_AdvancedBandNodes
+{
+    [DefOf]
+    public class AdvancedBandNodeDefOf
+    {
+        [MayRequireBiotech]
+        public static ThingDef Hydroxyapatite_AdvancedBandNode;
+        static AdvancedBandNodeDefOf()
+        {
+            DefOfHelper.EnsureInitializedInCtor(typeof(AdvancedBandNodeDefOf));
+        }
+    }
+}
