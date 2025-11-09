a75f261887d52d3dd7ec1f988bb2452e955e508b
Mlie <mlie@litet.net>
2025-11-09 18:10:58 +0100

    Body Size Genes

---
+++ 2887397385/Source/genebodysize/HarmonyPatches.cs
@@ -0,0 +1,37 @@
+﻿using HarmonyLib;
+using RimWorld;
+using VariedBodySizes;
+using Verse;
+
+// ReSharper disable InconsistentNaming
+
+namespace genebodysize;
+
+public class HarmonyPatches
+{
+    [HarmonyPatch(typeof(VariedBodySizes_GameComponent), "OnCalculateBodySize")]
+    public static class Pawn_BodySizePatch_OnCalculateBodySizePatch
+    {
+        private static readonly TimedCache<float> statCache = new(36);
+        private static StatDef bodySizeMultiplier;
+        // Register a modifier for ez access
+        public static void Postfix(ref float __result, Pawn pawn)
+        {
+            // GetStatValue will access needs, which if null, will break loading /tableflip
+            if (!ModsConfig.BiotechActive || pawn?.RaceProps.Humanlike != true || pawn.needs == null && !pawn.Dead)
+                return;
+
+            // cached value, or calculate/cache/return
+            if (statCache.TryGet(pawn, out var pawnSize) && pawnSize > 0.01f) {
+                __result = pawnSize;
+                return;
+            }
+        
+            // Load-time fun!
+            bodySizeMultiplier ??= StatDef.Named("bodySizeFactor");
+            var bodySizeMultiplierValue = pawn.GetStatValue(bodySizeMultiplier, cacheStaleAfterTicks: 360);
+            statCache.Set(pawn, __result *= bodySizeMultiplierValue);
+        }
+        
+    }
+}
---
+++ 2887397385/Source/genebodysize/Main.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using HarmonyLib;
+using Verse;
+
+namespace genebodysize;
+[StaticConstructorOnStartup]
+public static class Main
+{
+    static Main()
+    {
+        new Harmony("mute.genebodysize").PatchAll(Assembly.GetExecutingAssembly());
+    }
+}
