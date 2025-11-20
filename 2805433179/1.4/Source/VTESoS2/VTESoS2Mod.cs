95480d6f91c1057f0f1c075441ad912aedd02a84
Mlie <mlie@litet.net>
2025-11-20 14:11:26 +0100

    Vanilla Textures Expanded - Save Our Ship 2

---
+++ 2805433179/1.4/Source/VTESoS2/VTESoS2Mod.cs
@@ -0,0 +1,59 @@
+﻿using HarmonyLib;
+using RimWorld;
+using SaveOurShip2;
+using Verse;
+
+namespace VTESoS
+{
+    public class VTESoS2Mod : Mod
+    {
+        public static Harmony Harm;
+
+        public VTESoS2Mod(ModContentPack content) : base(content)
+        {
+            Harm = new Harmony("vanillaexpanded.vtexe.sos2");
+            LongEventHandler.ExecuteWhenFinished(delegate
+            {
+                Harm.Patch(AccessTools.Method(typeof(ShipInteriorMod2), nameof(ShipInteriorMod2.DefsLoaded)),
+                    new HarmonyMethod(GetType(), nameof(DefsLoaded_Prefix)), new HarmonyMethod(GetType(), nameof(DefsLoaded_Postfix)));
+            });
+        }
+
+        public static void DefsLoaded_Prefix(ref bool ___loadedGraphics, out bool __state)
+        {
+            __state = ___loadedGraphics;
+            ___loadedGraphics = true;
+        }
+
+        public static void DefsLoaded_Postfix(bool __state, ref bool ___loadedGraphics)
+        {
+            if (__state) return;
+            ___loadedGraphics = false;
+            foreach (var thingToResolve in CompShuttleCosmetics.GraphicsToResolve.Keys)
+            {
+                var graphicsResolved = new Graphic[CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics.Count];
+                var graphicsHoverResolved = new Graphic[CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover.Count];
+                for (var i = 0; i < CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics.Count; i++)
+                {
+                    var graphicData = CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphics[i];
+                    graphicsResolved[i] = GraphicDatabase.Get(graphicData.graphicClass, graphicData.texPath,
+                        (graphicData.shaderType ?? ShaderTypeDefOf.Cutout).Shader, graphicData.drawSize, graphicData.color, graphicData.colorTwo, graphicData,
+                        graphicData.shaderParameters);
+                }
+
+                for (var j = 0; j < CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover.Count; j++)
+                {
+                    var graphicData = CompShuttleCosmetics.GraphicsToResolve[thingToResolve].graphicsHover[j];
+                    graphicsHoverResolved[j] = GraphicDatabase.Get(graphicData.graphicClass, graphicData.texPath,
+                        (graphicData.shaderType ?? ShaderTypeDefOf.Cutout).Shader, graphicData.drawSize, graphicData.color, graphicData.colorTwo, graphicData,
+                        graphicData.shaderParameters);
+                }
+
+                CompShuttleCosmetics.graphics.Add(thingToResolve.defName, graphicsResolved);
+                CompShuttleCosmetics.graphicsHover.Add(thingToResolve.defName, graphicsHoverResolved);
+            }
+
+            ___loadedGraphics = true;
+        }
+    }
+}
