1a7578ff80f586e9da0cd8e65ef8f2783ed8bc0b
mod-archiver <archiver@noreply.local>
2025-12-21 20:13:56 +0100

    SOS2 & Dub's Hygiene Compatibility

---
+++ 1910681256/Source/RimworldMod/CompProperties_WaterRecycler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    class CompProperties_WaterRecycler : CompProperties
+    {
+        public int RecycleTicks;
+
+        public CompProperties_WaterRecycler()
+        {
+            this.compClass = typeof(CompWaterRecycler);
+        }
+    }
+}
---
+++ 1910681256/Source/RimworldMod/CompWaterRecycler.cs
@@ -0,0 +1,23 @@
+﻿using DubsBadHygiene;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Verse;
+
+namespace RimWorld
+{
+    class CompWaterRecycler : ThingComp
+    {
+        public override void CompTick()
+        {
+            base.CompTick();
+            if(this.parent.IsHashIntervalTick(((CompProperties_WaterRecycler)this.props).RecycleTicks))
+            {
+                CompWaterStorage storage = this.parent.TryGetComp<CompWaterStorage>();
+                if(storage != null && storage.CapPercent < 1)
+                    storage.WaterStorage++;
+            }
+        }
+    }
+}
