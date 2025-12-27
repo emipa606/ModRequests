b1b9dfccd7b38883dd4ebc5a7d5a838e35a0ec8c
mod-archiver <archiver@noreply.local>
2025-12-27 21:18:07 +0100

    Reuse Pods v1.5

---
+++ 2879963161/README.md
@@ -0,0 +1,8 @@
+# Reuse Pods
+Finally build the satellite colonies of your dreams without pouring endless steel and components into pod construction!
+
+**Author:** SomewhereOutInSpace
+
+**License:** MIT License
+
+[**Steam Workshop Page**](https://steamcommunity.com/sharedfiles/filedetails/?id=2549305885)
---
+++ 2879963161/Source/ReusePods/Building_PodFunnel.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace ReusePods {
+    public class Building_PodFunnel : Building
+    {
+
+        public override string GetInspectString()
+        {
+            string text = base.GetInspectString();
+            if (this.Position.Roofed(this.Map))
+            {
+                text = "Pod funnel offline - blocked by roof.";
+            }
+            else if (!this.Position.Roofed(this.Map)) {
+                text = "Pod funnel operational.";
+                text = text + "\nCurrently within funneling range of " + getServiceCount() + " pod launchers.";
+            }
+            return text;
+        }
+
+        private int getServiceCount() {
+            List<Building> outputList = new List<Building>();
+
+            foreach (Building item in this.Map.listerBuildings.AllBuildingsColonistOfDef(ThingDef.Named("PodLauncher")))
+            {
+                if (item.Position.DistanceTo(this.Position) <= 20f)
+                {
+                    outputList.Add(item);
+                }
+            }
+
+            if (Verse.ModLister.HasActiveModWithName("Vanilla Helixien Gas Expanded"))
+            {
+                foreach (Building item in this.Map.listerBuildings.AllBuildingsColonistOfDef(ThingDef.Named("VHGE_GasOperatedPodLauncher")))
+                {
+                    if (item.Position.DistanceTo(this.Position) <= 20f)
+                    {
+                        outputList.Add(item);
+                    }
+                }
+            }
+
+            if (Verse.ModLister.HasActiveModWithName("Electric Pod Launchers"))
+            {
+                foreach (Building item in this.Map.listerBuildings.AllBuildingsColonistOfDef(ThingDef.Named("PBL_ElectricPodLauncher")))
+                {
+                    if (item.Position.DistanceTo(this.Position) <= 20f)
+                    {
+                        outputList.Add(item);
+                    }
+                }
+            }
+
+
+            return outputList.Count;
+        }
+
+    }
+
+}
