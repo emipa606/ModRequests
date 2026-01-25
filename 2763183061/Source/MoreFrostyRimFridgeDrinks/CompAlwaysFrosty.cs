b2ac01a03a325338d1c0ed693b1e92f14ffc62fd
mod-archiver <archiver@noreply.local>
2026-01-25 08:53:47 +0100

    More Frosty RimFridge Drinks

---
+++ 2763183061/Source/MoreFrostyRimFridgeDrinks/CompAlwaysFrosty.cs
@@ -0,0 +1,26 @@
+﻿using RimFridge;
+using Verse;
+
+namespace MoreFrostyRimFridgeDrinks
+{
+    internal class CompAlwaysFrosty : CompFrosty
+    {
+        public override void PostIngested(Pawn ingester) 
+            => ingester.needs.mood.thoughts.memories.TryGainMemory(Props.thought);
+
+        public override void PostSpawnSetup(bool respawningAfterLoad)
+        {
+            base.PostSpawnSetup(respawningAfterLoad);
+
+            temperature = 0f;
+        }
+
+        public override void PostSplitOff(Thing piece)
+        { }
+
+        public override void CompTickRare()
+        { }
+
+        public override string CompInspectStringExtra() => "Frosty";
+    }
+}
---
+++ 2763183061/Source/MoreFrostyRimFridgeDrinks/MoreFrostyRimFridgeDrinksMod.cs
@@ -0,0 +1,12 @@
+﻿using JetBrains.Annotations;
+using Verse;
+
+namespace MoreFrostyRimFridgeDrinks
+{
+    [UsedImplicitly]
+    public class MoreFrostyRimFridgeDrinksMod : Mod
+    {
+        public MoreFrostyRimFridgeDrinksMod(ModContentPack content) : base(content)
+        { }
+    }
+}
