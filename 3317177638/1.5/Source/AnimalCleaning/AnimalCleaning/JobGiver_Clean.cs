333f0f13d3d4d447ada4944106c8e16731597c2a
mod-archiver <archiver@noreply.local>
2026-07-07 18:29:27 +0000

    pphhyy BoneReapers

---
+++ 3317177638/1.5/Source/AnimalCleaning/AnimalCleaning/JobGiver_Clean.cs
@@ -0,0 +1,64 @@
+﻿using Verse;
+using Verse.AI;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AnimalCleaning
+{
+    public class JobGiver_Clean : ThinkNode_JobGiver
+    {
+        protected override Job TryGiveJob(Pawn pawn)
+        {
+            bool validator(Thing t)
+            {
+                if (!pawn.CanReserve(t, 1, -1, null))
+                {
+                    return false;
+                }
+                return true;
+            }
+
+            if (pawn.Map.listerFilthInHomeArea.FilthInHomeArea.Count == 0)
+            {
+                return null;
+            }
+            Thing filth = GenClosest.ClosestThing_Global_Reachable(pawn.Position, pawn.Map, pawn.Map.listerFilthInHomeArea.FilthInHomeArea,
+                PathEndMode.OnCell, TraverseParms.For(pawn), radius, validator);
+
+            if (filth != null)
+            {
+                return CreateJob(pawn, filth);
+            }
+            return null;
+        }
+
+        public Job CreateJob(Pawn pawn, Thing initialFilth)
+        {
+            Job result = new Job(JobDefOf.Clean);
+            Map map = pawn.Map;
+            Room room = initialFilth.GetRoom();
+            IEnumerable<IntVec3> cells = GenRadial.RadialCellsAround(initialFilth.Position, radius, true);
+            foreach (IntVec3 cell in cells)
+            {
+                if (cell.InBounds(map) && cell.GetRoom(map) == room && cell.InAllowedArea(pawn))
+                {
+                    foreach (Thing thing in cell.GetThingList(map).Where(x => x is Filth))
+                    {
+                        if (pawn.CanReserve(thing, 1, -1, null) && thing.Map.areaManager.Home[thing.Position])
+                        {
+                            result.AddQueuedTarget(TargetIndex.A, thing);
+                        }
+                    }
+                }
+            }
+            if (result.targetQueueA != null && result.targetQueueA.Count >= 5)
+            {
+                result.targetQueueA.SortBy((LocalTargetInfo targ) => targ.Cell.DistanceToSquared(pawn.Position));
+            }
+            return result;
+        }
+
+        public float radius = 6f;
+    }
+}
---
+++ 3317177638/README.md
@@ -0,0 +1,2 @@
+# BoneReapers
+ BoneReapers
